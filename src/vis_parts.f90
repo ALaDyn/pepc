e15a47160764ee50aa63cb9d8c2857b8528a09bc
Paul Gibbon <p.gibbon@fz-juelich.de>
2003-11-03 14:07:35 +0000

    Variable particle number with relabelling; no tracking possible

--- src/vis_parts.f90
+++ src/vis_parts.f90
@@ -22,20 +22,28 @@ subroutine vis_parts
 
   integer, dimension(num_pe) :: nparts_pe  ! array of npp on each PE
   integer :: icolour(npart_visit_max)
-  integer :: lvisit_active, nskip, nproot, npart_buf
+  integer :: lvisit_active, nskip, nproot, npart_buf, ne_buf, ni_buf, nbufe, nbufi
   integer :: i, j, k, ioffset,ixd, iyd, izd, ilev, lcount
 
   real :: s, simtime, dummy, xd,yd,zd, dx, dz, dy, epond_max, box_max, epondx, epondy, epondz,phipond
+  real :: xl_mu, yl_mu, zl_mu, t_fs
 
   simtime = dt*(itime+itime_start)
   nskip = npart/npart_visit_max + 1
-!  nbuf = npp/nskip
+  !  nbuf = npp/nskip
 
- ! Filter out particles for shipping
-  nbuf = 0
+  ! Filter out particles for shipping
+  nbufe = 0
+  nbufi = 0
+  nbuf=0
   do i=1,npp
-!     if (q(i)<0 .and. y(i)<yl/2.) then  ! 1/2 box of electrons
+     if (q(i)<0 .or. (q(i)>0 .and. mod(itime,ivis*10).eq.0)) then  !  pick out even labels
         nbuf=nbuf+1
+        if (q(i)<0) then
+           nbufe=nbufe+1
+        else
+           nbufi=nbufi+1
+        endif
         ! scaled coordinates
         xv(nbuf) = convert_mu*x(i)
         yv(nbuf) = convert_mu*y(i)
@@ -44,30 +52,45 @@ subroutine vis_parts
         uyv(nbuf) = uy(i)
         uzv(nbuf) = uz(i)
         qv(nbuf) = q(i)
-!        mv(nbuf) = m(i)
+        !        mv(nbuf) = m(i)
         pepidv(nbuf) = pepid(i)
         pelabelv(nbuf) = pelabel(i)
-!     endif
+     endif
   end do
 
   call MPI_ALLGATHER( nbuf, 1, MPI_INTEGER, nparts_pe,1, MPI_INTEGER, MPI_COMM_WORLD, ierr )
   recv_strides(1:num_pe) = (/ 0,( SUM( nparts_pe(1:i-1) ),i=2,num_pe ) /)
-  npart_buf = SUM(nparts_pe)  ! total # particles to send
+  npart_buf = (SUM(nparts_pe))  ! total # particles to send
+  call MPI_ALLREDUCE( nbufe, ne_buf, 1, MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD, ierr )
+  call MPI_ALLREDUCE( nbufi, ni_buf, 1, MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD, ierr )
+
+  if (me==0) write(*,*) '# particles shipped ',npart_buf
 
   if (npart_buf<npart_visit_max) then
      ! send  particle data from all PEs
      if (me==0) then
+        xl_mu = int(xl*convert_mu*100)/100.
+        yl_mu = int(yl*convert_mu*100)/100.
+        zl_mu = int(zl*convert_mu*100)/100.
         call flvisit_spk_check_connection(lvisit_active)
-        call flvisit_spk_info_send(npart_buf,xl*convert_mu,yl*convert_mu,zl*convert_mu, &
-             zl*convert_mu,npart_buf,0,0,itime)
+        call flvisit_spk_info_send(npart_buf,xl_mu,yl_mu,zl_mu,zl_mu,ne_buf,ni_buf,np_beam,itime+itime_start)
+
+!        call flvisit_spk_info_send(npart_buf,xl_mu,yl_mu,zl_mu, zl_mu, &
+!             propag_laser*convert_mu, vosc, sigma, tpulse, &
+!             ne_buf,ni_buf,npart_buf,int(tlaser))
      endif
+!
+! visit info block format in spk4
+! int flvisit_spk_info_send(int *npart,double *xl,double *yl,double *zl,double *sbox,double *boxoffsetx,double *vosc,double *sigma,double *tphase,int *ne,int *ni,int *np,int *itime);
+
 
      ! work out stride lengths so that partial arrays placed sequentially in global array
 
 
      ! Gather particle data onto root PE
      call MPI_GATHERV( pepidv, nbuf, MPI_INTEGER, ppid, nparts_pe, recv_strides, MPI_INTEGER, root, MPI_COMM_WORLD, ierr )
-     call MPI_GATHERV( pelabelv, nbuf, MPI_INTEGER, plabel, nparts_pe, recv_strides, MPI_INTEGER, root, MPI_COMM_WORLD, ierr) 
+!     call MPI_GATHERV( pelabelv, nbuf, MPI_INTEGER, plabel, nparts_pe, recv_strides, MPI_INTEGER, root, MPI_COMM_WORLD, ierr) 
+     plabel(1:npart_buf) = (/ (i,i=1,npart_buf) /) ! Relabel 
      call MPI_GATHERV( xv, nbuf, MPI_REAL8, xvis, nparts_pe, recv_strides, MPI_REAL8, root, MPI_COMM_WORLD, ierr )
      call MPI_GATHERV( yv, nbuf, MPI_REAL8, yvis, nparts_pe, recv_strides, MPI_REAL8, root, MPI_COMM_WORLD, ierr )
      call MPI_GATHERV( zv, nbuf, MPI_REAL8, zvis, nparts_pe, recv_strides, MPI_REAL8, root, MPI_COMM_WORLD, ierr )
@@ -88,7 +111,7 @@ subroutine vis_parts
      if (me==0) then
         nproot = 0.8*npart/num_pe ! fixed # parts close to npp
         call flvisit_spk_check_connection(lvisit_active)
-        call flvisit_spk_info_send(npp,xl,yl,zl,zl,nep,nip,np_beam,itime+itime_start)
+!        call flvisit_spk_info_send(npp,xl,yl,zl,zl,nep,nip,np_beam,itime+itime_start)
         call flvisit_spk_particles_send(simtime,x,y,z,ux,uy,uz,q,pepid,pelabel,npp)
      endif
   endif
@@ -108,7 +131,7 @@ subroutine vis_parts
         zvis(j)=(zmin + izd*mvis(j))*convert_mu 
 
      end do
-!     call flvisit_spk_domains_send( simtime*convert_fs, xvis,yvis,zvis,mvis,branch_owner,xvis,nbranch_sum)
+     !     call flvisit_spk_domains_send( simtime*convert_fs, xvis,yvis,zvis,mvis,branch_owner,xvis,nbranch_sum)
 
   endif
 
