ac75f8701c6fe86bbb73118adc730c3f8a7464aa
Paul Gibbon <p.gibbon@fz-juelich.de>
2004-08-02 07:40:53 +0000

    fixed pepc path

--- tutorial/dust.sh
+++ tutorial/dust.sh
@@ -1,7 +1,7 @@
 echo "Starting dust  .."
 cp dust.h run.h
 #cp pe000/parts_info.000400 parts_info.in
-llrun -p8 $HOME/tree/pepc/src/pepc 
+llrun -p8 ../src/pepc 
  eval `cat runstamp |  awk '{ t = $1;  printf  "TEND=%s",t}'`
 cp energy.dat energy.$TEND
 cp run.out run.$TEND
--- tutorial/eqm.sh
+++ tutorial/eqm.sh
@@ -1,6 +1,6 @@
 echo "Starting clamped eqm ..."
 cp clamp.h run.h
-llrun -p4 $HOME/tree/pepc/src/pepc2 
+llrun -p4 ../src/pepc 
 eval `cat runstamp |  awk '{ t = $1;  printf  "TEND=%s",t}'`
 cp energy.dat log/energy.$TEND
 cp run.out log/run.$TEND
