4d1d3e9f54bdf4bfdb647e43d2fcb1af0621ae79
Paul Gibbon <p.gibbon@fz-juelich.de>
2004-11-24 11:18:20 +0000

    Cache optimization: multipole moment arrays copied into single, stride-1 matrix prior to sum loop

--- src/sum_force.f90
+++ src/sum_force.f90
@@ -13,12 +13,13 @@ subroutine sum_force( p, n, inode, eps, sumfx, sumfy, sumfz, sumphi )
   integer, intent(in) :: n  !  # terms on interaction list
   integer, dimension(1:n) ::  inode
   real, intent(in) :: eps ! smoothing parameter
-  integer :: jnode, i
+  integer :: jnode, i,j,k 
 
   real :: rd,dx,dy,dz,d,dx2,dy2,dz2 
  real :: dx3,dy3,dz3,rd3,rd5,rd7,fd1,fd2,fd3,fd4,fd5,fd6
  real :: fsx,fsy,fsz,phi
- real, dimension(n) :: q_loc, xdl, ydl, zdl, xxql, yyql, zzql, xyql, yzql, zxql 
+ real, dimension(n*10) :: mult 
+ real, dimension(n*3) :: coc
   real, intent(out) ::  sumfx,sumfy,sumfz,sumphi 
   real :: eps2
 
@@ -29,27 +30,34 @@ subroutine sum_force( p, n, inode, eps, sumfx, sumfy, sumfz, sumphi )
   sumphi = 0
 
 ! copy multipole moments into stride 1 array
-  do i=1,n
-    jnode=inode(i)
-    q_loc(i) = charge(jnode)
-    xdl(i) = xdip(jnode)
-    ydl(i) = ydip(jnode)
-    zdl(i) = zdip(jnode)
-    xxql(i) = xxquad(jnode)
-    yyql(i) = yyquad(jnode)
-    zzql(i) = zzquad(jnode)
-    xyql(i) = xyquad(jnode)
-    yzql(i) = yzquad(jnode)
-    zxql(i) = zxquad(jnode)
+  do j=1,n
+    jnode=inode(j)
+    i = (j-1)*10+1
+!    k = (j-1)*3+1
+!    coc(k) = xcoc(jnode)
+!    coc(k+1) = ycoc(jnode)
+!    coc(k+2) = zcoc(jnode)
+    mult(i) = charge(jnode)
+    mult(i+1) = xdip(jnode)
+    mult(i+2) = ydip(jnode)
+    mult(i+3) = zdip(jnode)
+    mult(i+4) = xxquad(jnode)
+    mult(i+5) = yyquad(jnode)
+    mult(i+6) = zzquad(jnode)
+    mult(i+7) = xyquad(jnode)
+    mult(i+8) = yzquad(jnode)
+    mult(i+9) = zxquad(jnode)
   end do
 
-  do i=1,n
+  do j=1,n
      
   !  preprocess distances
-     jnode = inode(i)
-     dx = x(p) - xcoc( jnode )
-     dy = y(p) - ycoc( jnode )
-     dz = z(p) - zcoc( jnode ) 
+     i = 10*(j-1) + 1  ! multipole index
+!     k = 3*(j-1) + 1  ! coc index
+     jnode=inode(j)
+     dx = x(p) - xcoc(jnode)
+     dy = y(p) - ycoc(jnode)
+     dz = z(p) - zcoc(jnode) 
 
      d = sqrt(dx**2+dy**2+dz**2+eps2)
      rd = 1./d
@@ -73,42 +81,45 @@ subroutine sum_force( p, n, inode, eps, sumfx, sumfy, sumfz, sumphi )
 
      ! potential
 
-     sumphi = sumphi + q_loc(i)*rd    &                           !  monopole term
+     sumphi = sumphi + mult(i)*rd    &                           !  monopole term
           ! 
-     + (dx*xdl(i) + dy*ydl(i) + dz*zdl(i))*rd3  &    !  dipole 
-          -0.5*fd1*xxql(i) - 0.5*fd2*yyql(i) - 0.5*fd3*zzql(i)  &  !  quadrupole
-          + fd4*xyql(i) + fd5*yzql(i) + fd6*zxql(i)      
+     + (dx*mult(i+1) + dy*mult(i+2) + dz*mult(i+3))*rd3  &    !  dipole 
+     !        Dx             Dy            Dz
+          -0.5*fd1*mult(i+4) - 0.5*fd2*mult(i+5) - 0.5*fd3*mult(i+6)  &  !  quadrupole
+          !           Qxx                 Qyy                 Qzz
+          + fd4*mult(i+7) + fd5*mult(i+8) + fd6*mult(i+9)      
+     !             Qxy            Qyz             Qzx
 
      !  forces
 
-     sumfx = sumfx + q_loc(i)*dx*rd3 &      ! monopole term
+     sumfx = sumfx + mult(i)*dx*rd3 &      ! monopole term
           !
-     - ( fd1*xdl(i) + fd4*ydl(i) + fd6*zdl(i) )  &   !  dipole term
+     - ( fd1*mult(i+1) + fd4*mult(i+2) + fd6*mult(i+3) )  &   !  dipole term
           !
-     + ( -9.*dx*rd5 + 15.*dx3*rd7 )*0.5*xxql(i) &
-          + ( -3.*dy*rd5 + 15.*dy*dx2*rd7 )*xyql(i) &
-          + ( -3.*dz*rd5 + 15.*dz*dx2*rd7 )*zxql(i) &   !   quadrupole term
-          + ( 15*dx*dy*dz*rd7 )*yzql(i) &
-          + ( -3.*dx*rd5 + 15.*dx*dy2*rd7 )*0.5*yyql(i) &
-          + ( -3.*dx*rd5 + 15.*dx*dz2*rd7 )*0.5*zzql(i) 
+     + ( -9.*dx*rd5 + 15.*dx3*rd7 )*0.5*mult(i+4) &
+          + ( -3.*dy*rd5 + 15.*dy*dx2*rd7 )*mult(i+7) &
+          + ( -3.*dz*rd5 + 15.*dz*dx2*rd7 )*mult(i+9) &   !   quadrupole term
+          + ( 15*dx*dy*dz*rd7 )*mult(i+8) &
+          + ( -3.*dx*rd5 + 15.*dx*dy2*rd7 )*0.5*mult(i+5) &
+          + ( -3.*dx*rd5 + 15.*dx*dz2*rd7 )*0.5*mult(i+6) 
 
-     sumfy = sumfy + q_loc(i)*dy*rd3 &
-          - ( fd2*ydl(i) + fd4*xdl(i) + fd5*zdl(i) ) &
-          + ( -9.*dy*rd5 + 15.*dy3*rd7 )*0.5*yyql(i) &
-          + ( -3.*dx*rd5 + 15.*dx*dy2*rd7 )*xyql(i) &
-          + ( -3.*dz*rd5 + 15.*dz*dy2*rd7 )*yzql(i) &
-          + ( 15.*dx*dy*dz*rd7 )*zxql(i) &
-          + ( -3.*dy*rd5 + 15.*dy*dx2*rd7 )*0.5*xxql(i) &
-          + ( -3.*dy*rd5 + 15.*dy*dz2*rd7 )*0.5*zzql(i) 
+     sumfy = sumfy + mult(i)*dy*rd3 &
+          - ( fd2*mult(i+2) + fd4*mult(i+1) + fd5*mult(i+3) ) &
+          + ( -9.*dy*rd5 + 15.*dy3*rd7 )*0.5*mult(i+5) &
+          + ( -3.*dx*rd5 + 15.*dx*dy2*rd7 )*mult(i+7) &
+          + ( -3.*dz*rd5 + 15.*dz*dy2*rd7 )*mult(i+8) &
+          + ( 15.*dx*dy*dz*rd7 )*mult(i+9) &
+          + ( -3.*dy*rd5 + 15.*dy*dx2*rd7 )*0.5*mult(i+4) &
+          + ( -3.*dy*rd5 + 15.*dy*dz2*rd7 )*0.5*mult(i+6) 
 
-     sumfz = sumfz + q_loc(i)*dz*rd3 &
-          - ( fd3*zdl(i) + fd5*ydl(i) + fd6*xdl(i) ) &
-          + ( -9.*dz*rd5 + 15.*dz3*rd7 )*0.5*zzql(i) &
-          + ( -3.*dx*rd5 + 15.*dx*dz2*rd7 )*zxql(i) &
-          + ( -3.*dy*rd5 + 15.*dy*dz2*rd7 )*yzql(i) &
-          + ( 15.*dx*dy*dz*rd7 )*xyql(i) &
-          + ( -3.*dz*rd5 + 15.*dz*dy2*rd7 )*0.5*yyql(i) &
-          + ( -3.*dz*rd5 + 15.*dz*dx2*rd7 )*0.5*xxql(i) 
+     sumfz = sumfz + mult(i)*dz*rd3 &
+          - ( fd3*mult(i+3) + fd5*mult(i+2) + fd6*mult(i+1) ) &
+          + ( -9.*dz*rd5 + 15.*dz3*rd7 )*0.5*mult(i+6) &
+          + ( -3.*dx*rd5 + 15.*dx*dz2*rd7 )*mult(i+9) &
+          + ( -3.*dy*rd5 + 15.*dy*dz2*rd7 )*mult(i+8) &
+          + ( 15.*dx*dy*dz*rd7 )*mult(i+7) &
+          + ( -3.*dz*rd5 + 15.*dz*dy2*rd7 )*0.5*mult(i+5) &
+          + ( -3.*dz*rd5 + 15.*dz*dx2*rd7 )*0.5*mult(i+4) 
 
   end do
 
