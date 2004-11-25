dad1d7ef7b9b5731137702ba97371d7a9871c5ee
Paul Gibbon <p.gibbon@fz-juelich.de>
2004-11-25 07:24:35 +0000

    Removed demo_wake

--- demo_wake/run.h
+++ demo_wake/run.h
@@ -1,13 +1,13 @@
- &pepcdata
- nmerge =-1 
+  &pepcdata
+ nmerge = -1
  !perf_anal=.true.
  domain_debug=.false.
  load_balance=.true.
  walk_balance=.true.
  !walk_debug=.true.
 ! particles
-  ne = 50000
-  ni = 50000 
+  ne = 320000
+  ni = 320000 
 
 !  initial_config = 1   ! sphere
    initial_config = 2         ! random cylinder 
@@ -18,27 +18,27 @@
 
 ! physics stuff
 
-  theta = 0.7
-  Te_keV = 0.1 ! Temperatures in keV
+  theta = 0.75
+  Te_keV = 0.3 ! Temperatures in keV
   Ti_keV =0.2 
-  mass_ratio = 2000.
+  mass_ratio = 100.
   q_factor = 1.
 
   r_sphere = 7
-  x_plasma = 25.    ! plasma disc thickness/ wire length
-  y_plasma = 15.     ! plasma width (slab target)
-  z_plasma = 15.
-  xl = 30  ! graphics box size
-  yl =14 
-  zl =14 
-  ngx = 80
-  ngy = 20
-  ngz = 20
+  x_plasma = 40.    ! plasma disc thickness/ wire length
+  y_plasma = 20.     ! plasma width (slab target)
+  z_plasma = 20.
+  xl = 45  ! graphics box size
+  yl =20 
+  zl =20 
+  ngx = 100
+  ngy = 41
+  ngz = 41
 
 ! beam
   !  beam_config = 1  ! fixed beam, initialised at start
  ! beam_config = 2  ! user-controlled, real-time particle source
- !  beam_config = 0 ! beam off
+!   beam_config = 0 ! beam off
   beam_config=5  ! laser bullet (propagating fpond) 
  
 
@@ -53,25 +53,25 @@
 
   np_beam = 0 ! initial # beam particles/ dt
 
-  vosc = 0.4
+  vosc = 2.0
   omega = 5.
   sigma = 5.
-  tpulse = 2.
+  tpulse = 3.
   x_offset = -3. ! centre of pulse relative to plasma edge
   lambda = 1.0   ! Wavelength in microns
 
   ! control
   nt =1000
-  dt = 0.196
+  dt = 0.5
   eps = 3.
  restart = .true.
   vis_on = .true.
  steering = .true.
- ivis = 2
- ivis_fields = 2
-  idump = 500000
-  iprot=10
+ ivis = 5
+ ivis_fields = 5
+  idump = 10000
+  iprot=20
   idens=50
+  ivis_skip=4
   ensemble = 1 /
-
 
