117952b67908986660eeba2233a3e44505324bc0
Paul Gibbon <p.gibbon@fz-juelich.de>
2005-05-12 06:36:47 +0000

    New vis parts for xnbody

---
+++ src/vis_parts_nbody.f90
@@ -0,0 +1,202 @@
+! ======================
+!
+!   VIS_PARTS
+!
+!   Send particle data to VISIT for real-time visualisation
+!
+!
+! ======================
+
+subroutine vis_parts_nbody
+
+
+  use physvars
+  use treevars
+  implicit none   
+
+  integer, parameter :: npart_visit_max = 10000  ! Max 250k data points for VIS
+  integer, parameter :: ship_max = 10000, attrib_max=22
+  real, dimension(0:attrib_max-1,npart_visit_max) :: vbuffer
+  real, dimension(0:attrib_max-1,nppm) :: vbuf_local
+
+  integer, dimension(num_pe) :: nparts_pe, recv_strides, nbuf_pe  ! array of npp on each PE
+  integer :: icolour(npart_visit_max)
+  integer :: lvisit_active, nskip, nproot, ne_buf, ni_buf, npart_buf, nbufe, nbufi
+  integer :: i, j, k, ioffset,ixd, iyd, izd, ilev, lcount, wfdatai
+
+  real :: s, simtime, dummy, xd,yd,zd, dx, dz, dy, epond_max, box_max, epondx, epondy, epondz,phipond
+  real :: plasma1, plasma2, plasma3, t_display, wfdatar, u2, amp_las
+  integer :: nship, ierr
+  integer :: type
+
+  convert_mu=1.
+  simtime = dt*(itime+itime_start)
+
+  nskip = npart/ship_max + 1
+  if (beam_config==4) then
+     amp_las = vosc*min(1.,simtime/tpulse)
+  else 
+     amp_las = vosc
+  endif
+
+  if (beam_config>=3 .and. beam_config<=5) then
+     t_display = tlaser*convert_fs
+  else
+     t_display = simtime
+  endif
+
+  if (beam_config<=3) then
+     plasma1 = Ukine*convert_keV
+     plasma2 = Ukini*convert_keV
+  else
+     plasma1=sigma
+     plasma2=tpulse
+  endif
+
+  if (nskip>1 .and. uthresh==-1) uthresh=vte**2
+
+  ! Filter out particles for shipping
+  nbufe = 0
+  nbufi = 0
+  nship=0
+  type = 1
+! TODO:  separate interesting ions and electrons
+  do i=1,npp
+     u2=0.5*0.511*mass_ratio*(ux(i)**2+uy(i)**2+uz(i)**2) ! in MeV
+
+!    if ((npart>=100000 .and. (u2>uthresh .and. q(i)<0)) .or. (npart<100000 .and. mod(pelabel(i),nskip).eq.0)) then
+        nship=nship+1
+        nbufe=nbufe+1
+!        nbufi=nbufi+1
+
+        !        else if (q(i)>0 .and. mod(pelabel(i),nskip).eq.0) then
+        !           nbufi=nbufi+1
+        !          nship=nship+1
+! Store attributes for visualizing
+     vbuf_local(0,nship) = t_display
+     vbuf_local(1,nship) = dt
+     ! scaled coordinates
+     vbuf_local(2,nship) = convert_mu*x(i)
+     vbuf_local(3,nship) = convert_mu*y(i)
+     vbuf_local(4,nship) = convert_mu*z(i)
+! velocities
+     vbuf_local(5,nship) = ux(i)
+     vbuf_local(6,nship) = uy(i)
+     vbuf_local(7,nship) = uz(i)
+! E-field
+     vbuf_local(8,nship) = Ex(i)
+     vbuf_local(9,nship) = Ey(i)
+     vbuf_local(10,nship) = Ez(i)
+     vbuf_local(11,nship) = 0.
+     vbuf_local(12,nship) = 0.
+     vbuf_local(13,nship) = 0.
+     vbuf_local(14,nship) = pepid(i)
+     vbuf_local(15,nship) = pelabel(i)
+     vbuf_local(16,nship) = type ! particle type
+     vbuf_local(17,nship) = q(i)
+     vbuf_local(18,nship) = m(i)
+     vbuf_local(19,nship) = 0.
+     vbuf_local(20,nship) = 0.
+     vbuf_local(21,nship) = 0.
+!   endif
+  end do
+
+! Find # particles to collect from each cpu
+  call MPI_ALLGATHER( nship, 1, MPI_INTEGER, nparts_pe,1, MPI_INTEGER, MPI_COMM_WORLD, ierr )
+
+! Total buffer lengths/pe
+  nbuf_pe=nparts_pe*attrib_max
+
+  recv_strides(1:num_pe) = (/ 0,( SUM( nbuf_pe(1:i-1) ),i=2,num_pe ) /)
+
+  npart_buf = (SUM(nparts_pe))  ! total # particles to send
+
+  call MPI_ALLREDUCE( nbufe, ne_buf, 1, MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD, ierr )
+  call MPI_ALLREDUCE( nbufi, ni_buf, 1, MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD, ierr )
+
+  if (me==0) then 
+	write(*,*) '# particles shipped ',npart_buf
+	write(*,*) 'u_thresh: (MeV)     ',uthresh
+  endif
+  if (npart_buf>0 .and. npart_buf < 2*ship_max) then
+     ! send  particle data from all PEs
+     ! increase momentum threshold if close to max ship # 
+     if (1.0*npart_buf/ship_max > 0.9) uthresh=uthresh*1.1
+     if (me==0) then
+
+! 20 real info parameters
+       call flvisit_nbody2_info_send(t_display,xl,yl,zl, xl, &
+             t_display, amp_las, plasma1, plasma2, xl, &
+             xl, xl, xl, xl, xl, & 
+             xl, xl, xl, xl, xl )
+
+     endif
+
+! Gather particle data onto root PE
+
+     call MPI_GATHERV( vbuf_local, nship*attrib_max, MPI_REAL8, vbuffer, nbuf_pe, recv_strides, MPI_REAL8, 0, MPI_COMM_WORLD, ierr )
+
+ 
+! Ship to visualization
+     if (me.eq.0) then
+        call flvisit_nbody2_partstep_send(vbuffer,nship,attrib_max)
+     endif
+
+  else
+     ! Just send particles on root
+     if (me==0) then
+        if (npart_buf>2*ship_max) then
+           write(*,*) 'Too many particles to ship - sending ones on root'
+           uthresh = uthresh*2
+           write(*,*) 'Increasing momentum threshold to: ',sqrt(abs(uthresh))
+        else if (npart_buf==0 .and. ne > 0) then
+           uthresh = vte*2
+           write(*,*) 'Reducing momentum threshold to: ',sqrt(abs(uthresh))
+        endif
+           nproot = 0.8*npart/num_pe ! fixed # parts close to npp
+
+!           call flvisit_spk_check_connection(lvisit_active)
+           call flvisit_nbody2_check_connection(lvisit_active)
+           !        call flvisit_spk_info_send(npp,xl,yl,zl,zl,nep,nip,np_beam,itime+itime_start)
+           wfdatai=int(tlaser)
+           wfdatar=tlaser
+!           call flvisit_spk_info_send(nproot,xl,yl,zl, t_display, &
+!                x_crit, amp_las, sigma, tpulse, &
+!                ne,ni,npart,wfdatai)
+!           call flvisit_nbody2_info_send(t_display,xl,yl,zl, xl, &
+!             t_display, amp_las, plasma1, plasma2, xl, &
+!             xl, xl, xl, xl, xl, & 
+!             xl, xl, xl, xl, xl )
+!           call flvisit_spk_particles_send(t_display,x,y,z,ux,uy,uz,q,pepid,pelabel,npp)
+!        call flvisit_nbody2_particles_send(t_display,xvis,yvis,zvis,vx,vy,vz,qvis,ppid,plabel,npart_buf)
+        endif
+     endif
+
+
+     if (me==0) then
+
+        ! ship branch nodes to show domains
+        do j=1,nbranch_sum
+           ilev = log( 1.*branch_key(j) )/log(8.)
+           ixd = SUM( (/ (2**i*ibits( branch_key(j),3*i,1 ), i=0,ilev-1) /) )
+           iyd = SUM( (/ (2**i*ibits( branch_key(j),3*i+1,1 ), i=0,ilev-1) /) )
+           izd = SUM( (/ (2**i*ibits( branch_key(j),3*i+2,1 ), i=0,ilev-1) /) )
+!           mvis(j) = boxsize/2**(ilev)          !  box length
+!           xvis(j)=(xmin + (ixd+0.05)*mvis(j))*convert_mu ! corners?
+!           yvis(j)=(ymin + (iyd+0.05)*mvis(j))*convert_mu
+!           zvis(j)=(zmin + (izd+0.05)*mvis(j))*convert_mu 
+!           mvis(j) = mvis(j)*0.9
+        end do
+!        call flvisit_spk_domains_send( tlaser, xvis,yvis,zvis,mvis,branch_owner,xvis,nbranch_sum)
+
+     endif
+
+! Make sure everyone else knows about new momentum threshold
+     call MPI_BCAST( uthresh, 1, MPI_REAL8, 0, MPI_COMM_WORLD,ierr)
+
+     call MPI_BARRIER( MPI_COMM_WORLD, ierr)  ! Wait for everyone to catch up
+
+
+   end subroutine vis_parts_nbody
+
+
