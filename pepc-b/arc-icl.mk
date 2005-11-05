c29360b9797d4c916f9a1e4c5dd04aec51d2e346
Paul Gibbon <p.gibbon@fz-juelich.de>
2005-11-05 12:12:24 +0000

    Make for Plasma Group Beowulf

---
+++ pepc-b/arc-icl.mk
@@ -0,0 +1,76 @@
+#  --------------------------------------------------------
+#
+#  Makefile Compiler and Library options for Beowulf cluster 
+#   'arc', in Plasma Group, ICL 
+#
+#  ---------------------------------------------------------
+
+#  Compilers including MPICH wrappers
+# full MPI path usually something like /usr/local/mpich/bin
+
+
+CC          = mpicc
+CCC         = mpicxx
+F77         = mpif77
+CLINKER     = mpicc
+CCLINKER    = mpicxx
+FLINKER     = mpif77
+F90         = mpif90
+F90LINKER   = mpif90
+FC = $(F90)
+CPP = /lib/cpp
+
+# KOJAK instrumentation
+#FC = mpxlf90_r -qdebug=function_trace
+#FC = kinst mpxlf90_r
+#FC = kinst-pomp -rcfile $(PREFIX)/pepc-b/opari.rc -- mpxlf90_r
+
+#  Library Archiver commands
+RANLIB  = ranlib
+AR      = ar
+
+#  Put compiler optimisation in here
+#  Lahey/Fujitsu compiler
+QTUNE = -O
+CFLAGS1= -O3 -g
+FFLAGS1 = --dbl $(QTUNE)
+#  Debug options
+#DB = -g --trace --ap
+
+
+#       LIBRARIES
+#   ---------------------
+
+#  PEPC library location
+
+LIBPEPC = -L../lpepcsrc -llpepc
+
+
+#  IBM library for timing routines
+
+#IBMLIB = -lxlf90
+
+#  MPI libraries - should be contained in wrappers
+
+LIBS_MPI =
+LIBSF_MPI =
+
+MPITRACE =
+#MPITRACE = -L/bgl/local/lib -lmpihpm_f -lbgl_perfctr.rts #for MPI+HPM information
+
+
+# Visit libraries
+
+# Setup flags for C-preprocessor
+# Use VISIT routines with XNBODY visualisation
+#PREPROC = -DVISIT_NBODY
+PREPROC = -UVISIT_NBODY
+
+VPREFIX=/tmp/demo/xnbody
+APISDIR=$(VPREFIX)/apis
+
+#VISITLIBS=-L$(VPREFIX)/apis/nbody3 -llvisit_nbody2 -L$(VPREFIX)/lvisit/lib -llvisit -L$(VPREFI
+X)/visit/lib -lvisit
+#NETCDFLIB = -lnetcdf
+
+
