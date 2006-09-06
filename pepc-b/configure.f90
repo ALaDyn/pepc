dbe6e6b36db30b193580a8a4222f0de0ec2cc8a7
Paul Gibbon <p.gibbon@fz-juelich.de>
2006-09-06 08:59:38 +0000

    New config: shell + proton disc

--- pepc-b/configure.f90
+++ pepc-b/configure.f90
@@ -383,6 +383,72 @@ subroutine configure
                 call add_ramp(x_plasma)     ! add exponential ramp to target (stretch container)
             endif
 
+            !###########################################################################################################
+
+        case(16)  ! Add proton disc to bowl
+            !    ====================================
+
+            write(ipefile,'(/a/)') "Setting up bowl"
+
+            target_geometry=36
+            velocity_config=1
+            plasma_centre =  (/ xl/2., yl/2., zl/2. /) 
+            offset_e = me*nep + ne_rest
+            offset_i = ne + me*nip + ni_rest
+
+            ! Electrons 
+            call plasma_start( 1, nep, ne, offset_e, target_geometry, velocity_config, idim, &
+                -rho0, -1.0, 1.0, vte, x_plasma, y_plasma, z_plasma, r_sphere, plasma_centre, &
+                number_faces, Vplas, Aplas, Qplas, qe, mass_e, a_ee )
+            ! Ions
+            call plasma_start( nep+1, nip, ni, offset_i, target_geometry, velocity_config, idim, &
+                rho0, 1.0, mass_ratio, vti, x_plasma, y_plasma, z_plasma, r_sphere, plasma_centre, &
+                number_faces, Vplas, Aplas, Qplas, qi, mass_i, a_ii )
+
+            ! Proton disc 
+
+            write(ipefile,'(/a/)') "Setting up proton disc"
+
+            ! Adjust local numbers if total non-multiple of # PEs
+            if (my_rank==0) then
+     		np_rest = mod(n_layer(1),n_cpu)
+            else
+     		np_rest = 0
+            endif
+
+            nlayp = n_layer(1)/n_cpu + np_rest  ! total # protons on this CPU
+            nep0 = nlayp
+            !  Make particle numbers on root known (in case of unequal particle #s - need for label offset)
+            call MPI_BCAST( nep0, 1, MPI_INTEGER, 0, MPI_COMM_WORLD,ierr)
+            ne_rest = nep0-nlayp
+            ni_rest = nep0-nlayp
+
+            ipstart = nep+nip+1
+            label_offset = ne+ni+me*nlayp +n_layer(1) + ni_rest
+
+            ! Place on rear (inside) surface of shell  
+            displace = (/ x_plasma+x_layer(1)/2-r_sphere/2.,0.,0. /)
+            !	layer_geometry = 2  ! disc
+
+            call plasma_start( ipstart, nlayp, n_layer(1), label_offset, layer_geometry, velocity_config, idim, &
+                rho_layer(1), 1.0, mratio_layer(1), vti, x_layer(1), y_layer(1), z_layer(1), r_layer(1), plasma_centre+displace, &
+                faces(1), V_layer(1), A_layer(1), Q_layer(1), qpart_layer(1), mass_layer(1), ai_layer(1) )
+
+            ! Equal number of neutralising electrons 
+            label_offset = ne+ni+me*nlayp + ne_rest 
+            call plasma_start( ipstart+nlayp, nlayp, n_layer(1), label_offset, layer_geometry, velocity_config, idim, &
+                -rho_layer(1), -1.0, 1.0, vte, x_layer(1), y_layer(1), z_layer(1), r_layer(1), plasma_centre+displace, &
+                faces(1), V_layer(1), A_layer(1), Q_layer(1), qpart_layer(1), mass_layer(1), ai_layer(1) )
+
+            npp=npp + 2*nlayp  ! Total # local particles
+            ne = ne + n_layer(1)  ! Global # particles
+            ni = ni + n_layer(1)  ! Global # particles
+            npart = ni+ne
+
+            if (scheme /= 5 .and. ramp) then
+                call add_ramp(x_plasma)     ! add exponential ramp to target (stretch container)
+            endif
+
 
         case(20)  ! disc + tube: inset target
             !    ====================================
