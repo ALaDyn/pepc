ec78b23f8388d92fdb86fdf4d05812eaf34bf72a
Paul Gibbon <p.gibbon@fz-juelich.de>
2008-01-22 07:49:58 +0000

    Tidied up microdot config 35

--- pepc-b/config_private.h
+++ pepc-b/config_private.h
@@ -303,9 +303,9 @@
 
 
 
-            !####################################################################################################
-        case(12)  ! A.P.L.R's set-up (8th March 2006)
-            !=====================================================
+            ! ####################################################################################################
+        case(12)  ! A.P.L.R  set-up (8th March 2006)
+            ! =====================================================
 
             target_geometry=0
             velocity_config=1
@@ -462,11 +462,83 @@
             endif
 
 
+	    case(14)  ! Add mixed layer to slab OUTSIDE the target (as 35)
+            !    ====================================
+
+            target_geometry=0
+            velocity_config=1
+            plasma_centre =  (/ xl/2., yl/2., zl/2. /) 
+            offset_e = me*nep + ne_rest
+            offset_i = ne + me*nip + ni_rest
+
+            ! Electrons 
+            call plasma_start( 1, nep, ne, offset_e, target_geometry, velocity_config, idim, &
+                -rho0, -1.0, 1.0, vte, x_plasma, y_plasma, z_plasma, r_sphere, plasma_centre, &
+                number_faces, Vplas, Aplas, Qplas, qe, mass_e, a_ee )
+            ! Ions
+            call plasma_start( nep+1, nip, ni, offset_i, target_geometry, velocity_config, idim, &
+                rho0, 1.0, mass_ratio, vti, x_plasma, y_plasma, z_plasma, r_sphere, plasma_centre, &
+                number_faces, Vplas, Aplas, Qplas, qi, mass_i, a_ii )
+
+            ! Microdot
+            ! Adjust local numbers if total non-multiple of # PEs
+            if (my_rank==0) then
+     		np_rest = mod(n_layer(1),n_cpu)
+            else
+     		np_rest = 0
+            endif
+
+            nlayp = n_layer(1)/n_cpu + np_rest  ! total # protons on this CPU
+            nep0 = nlayp
+            !  Make particle numbers on root known (in case of unequal particle #s - need for label offset)
+            call MPI_BCAST( nep0, 1, MPI_INTEGER, 0, MPI_COMM_WORLD,ierr)
+            ne_rest = nep0-nlayp
+            ni_rest = nep0-nlayp
+
+            ipstart = nep+nip+1
+
+            ! Place on REAR of main slab 
+
+            displace = (/ x_plasma/2.+x_layer(1)/2,0.,0. /)
+            label_offset = ne+ni+me*nlayp+ni_rest 
+
+            ! Equal number of neutralising electrons 
+            call plasma_start( ipstart+nlayp, nlayp, n_layer(1), label_offset, target_geometry, velocity_config, idim, &
+                -rho_layer(1), -1.0, 1.0, vte, x_layer(1), y_layer(1), z_layer(1), r_layer(1), plasma_centre+displace, &
+                faces(1), V_layer(1), A_layer(1), Q_layer(1), qpart_layer(1), mass_layer(1), ai_layer(1) )
+
+            label_offset = ne+ni+n_layer(1) +me*nlayp + ne_rest 
+            call plasma_start( ipstart, nlayp, n_layer(1), label_offset, target_geometry, velocity_config, idim, &
+                rho_layer(1), 1.0, mratio_layer(1), vti, x_layer(1), y_layer(1), z_layer(1), r_layer(1), plasma_centre+displace, &
+                faces(1), V_layer(1), A_layer(1), Q_layer(1), qpart_layer(1), mass_layer(1), ai_layer(1) )
+
+!  Turn 1/2 of ions into protons - assumes 50:50 mix @ same density
+            do i=ipstart,ipstart+nlayp,2
+                m(i) = m(i)/mratio_layer(1)*mass_proton
+            end do
+
+            if (debug_level==2 .and. me==0) then
+                write(*,*) "microdot ion charge ",qpart_layer(1)
+                write(*,*) "microdot ion mass ",mass_layer(1)
+  	        write(*,*) "microdot proton mass ",mass_layer(1)/mratio_layer(1)*mass_proton
+                write(*,*) "microdot spacing",ai_layer(1)
+            endif
+
+
+            npp=npp + 2*nlayp  ! Total # local particles
+            ne = ne + n_layer(1)  ! Global # particles
+            ni = ni + n_layer(1)  ! Global # particles
+            npart = ni+ne
+
+            if (scheme /= 5 .and. ramp) then
+                call add_ramp(x_plasma)     ! add exponential ramp to target (stretch container)
+            endif
 
 
 
 
-        case(32)  ! A.P.L.R's SECOND set-up (8th March 2006)
+
+        case(32)  ! A.P.L.Rs SECOND set-up (8th March 2006)
             !=====================================================
 
             target_geometry=0
