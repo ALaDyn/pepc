be8245828044b93e37caa5cc375f473fce329bbd
Lukas Arnold <l.arnold@fz-juelich.de>
2010-11-29 16:52:20 +0000

    final adjustments, reference trajectories included

--- jube/benchmark/run/check_results_pepc.pl
+++ jube/benchmark/run/check_results_pepc.pl
@@ -13,26 +13,48 @@ my $xmloutfile = $ARGV[0];
 my $vcheck=0;
 my $vcomment="not tested";
 
-my $limit=1e-6;
+my $limit=1e-9;
 
 my $line1; 
 my $line2;
 
-my $L2norm = 0;
+my $L2norm = (1.234e-3)*$limit;
 
 open(file1, "$ARGV[1]") or die "can not open file $ARGV[1]";
 open(file2, "$ARGV[2]") or die "can not open file $ARGV[2]";
 
 print "compare trajectories in $ARGV[1] and $ARGV[2]\n";
 
+$line1 = readline(file1);
+$line2 = readline(file2);
+my @values1 = split(/\s+/, $line1);
+my @values2 = split(/\s+/, $line2);
+
+if($values1[2] != $values2[2]) 
+{
+    $L2norm = $L2norm + 1.0e6;
+}
+print "total particles: $values1[2]\n";
+
+$line1 = readline(file1);
+$line2 = readline(file2);
+@values1 = split(/\s+/, $line1);
+@values2 = split(/\s+/, $line2);
+if($values1[2] != $values2[2]) 
+{
+    $L2norm = $L2norm + 1.0e6;
+}
+print "diag particles: $values1[2]\n";
+
+my $lines_tocompare = $values1[2];
+
 readline(file1);
 readline(file2);
 
-my $lines_compared = 0;
 while( defined ($line1 = readline(file1)) && defined ($line2 = readline(file2)) )
 {
-    print "line1: $line1";
-    print "line2: $line2";
+    print "line1: $line1\n";
+    print "line2: $line2\n";
 
     my @values1 = split(/\s+/, $line1);
     my @values2 = split(/\s+/, $line2);
@@ -44,18 +66,16 @@ while( defined ($line1 = readline(file1)) && defined ($line2 = readline(file2)) 
     print "linediff: $diffx $diffy $diffz\n";
     print "\n";
 
-    $L2norm = $L2norm + $diffx*$diffx + $diffy*$diffy + $diffz*$diffz;
+    $L2norm = $L2norm + sqrt($diffx*$diffx + $diffy*$diffy + $diffz*$diffz);
     
-    $lines_compared += 1;
+    $lines_tocompare -= 1;
 }
 
-if( $lines_compared != 4)
+if( $lines_tocompare != 0)
 {
-    print "not enough lines compared";
-    $L2norm = 10*$limit*$limit;
+    print "not enough lines compared\n";
+    $L2norm = 1e6+$limit;
 }
-
-$L2norm = sqrt($L2norm);
 
 print "L2norm difference $L2norm\n";
 
@@ -64,12 +84,12 @@ close(file2);
 
 if( $L2norm < $limit )
 {
-    $vcomment = "verify ok";
+    $vcomment = sprintf("verify ok (%6.4e)", $L2norm);
     $vcheck   = 1;
 }
 else
 {
-    $vcomment = "verify ERROR";
+    $vcomment = sprintf("verify ERROR (%6.4e)", $L2norm);
     $vcheck   = 0;
 }   
 
