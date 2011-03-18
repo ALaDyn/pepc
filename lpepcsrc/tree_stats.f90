4ab9a3ccd430e9bb87b502eed8905aaf8b2888d4
Mathias Winkel <m.winkel@fz-juelich.de>
2011-03-18 09:59:32 +0000

    extended stats.xxxx output by some values

--- lpepcsrc/tree_stats.f90
+++ lpepcsrc/tree_stats.f90
@@ -11,6 +11,8 @@
 subroutine tree_stats(timestamp)
 
   use treevars
+  use tree_walk_communicator
+  use tree_walk_utils
   implicit none
   include 'mpif.h'
 
@@ -18,22 +20,22 @@ subroutine tree_stats(timestamp)
   integer, dimension(num_pe) :: particles, fetches, ships, total_keys, tot_nleaf, tot_ntwig
   character*6 :: cdump
   character*40 :: cfile
-  integer :: max_fetches, max_nbranch, gmax_leaves, gmax_twigs, total_part
+  integer :: max_nbranch,min_nbranch, gmax_leaves, gmax_twigs, total_part
   real :: total_work, average_work
 
   ! particle distrib
-  call MPI_GATHER(npp, 1, MPI_INTEGER, particles, 1, MPI_INTEGER, 0,  MPI_COMM_WORLD, ierr )
-  call MPI_GATHER(ntwig_me, 1, MPI_INTEGER, tot_ntwig, 1, MPI_INTEGER, 0,  MPI_COMM_WORLD, ierr )
-  call MPI_GATHER(nleaf_me, 1, MPI_INTEGER, tot_nleaf, 1, MPI_INTEGER, 0,  MPI_COMM_WORLD, ierr )
+  call MPI_GATHER(npp,         1, MPI_INTEGER, particles,  1, MPI_INTEGER, 0,  MPI_COMM_WORLD, ierr )
+  call MPI_GATHER(ntwig_me,    1, MPI_INTEGER, tot_ntwig,  1, MPI_INTEGER, 0,  MPI_COMM_WORLD, ierr )
+  call MPI_GATHER(nleaf_me,    1, MPI_INTEGER, tot_nleaf,  1, MPI_INTEGER, 0,  MPI_COMM_WORLD, ierr )
   call MPI_GATHER(nkeys_total, 1, MPI_INTEGER, total_keys, 1, MPI_INTEGER, 0,  MPI_COMM_WORLD, ierr )
-  call MPI_GATHER(sum_fetches, 1, MPI_INTEGER, fetches, 1, MPI_INTEGER, 0,  MPI_COMM_WORLD, ierr )
-  call MPI_GATHER(sum_ships, 1, MPI_INTEGER, ships, 1, MPI_INTEGER, 0,  MPI_COMM_WORLD, ierr )
-  call MPI_GATHER(work_local, 1, MPI_REAL, work_loads, 1, MPI_REAL, 0,  MPI_COMM_WORLD, ierr )
-  call MPI_ALLREDUCE(sum_fetches, max_fetches, 1, MPI_INTEGER, MPI_MAX, MPI_COMM_WORLD, ierr )
-  call MPI_ALLREDUCE(nbranch, max_nbranch, 1, MPI_INTEGER, MPI_MAX, MPI_COMM_WORLD, ierr )
+  call MPI_GATHER(sum_fetches, 1, MPI_INTEGER, fetches,    1, MPI_INTEGER, 0,  MPI_COMM_WORLD, ierr )
+  call MPI_GATHER(sum_ships,   1, MPI_INTEGER, ships,      1, MPI_INTEGER, 0,  MPI_COMM_WORLD, ierr )
+  call MPI_GATHER(work_local,  1, MPI_REAL, work_loads,    1, MPI_REAL,    0,  MPI_COMM_WORLD, ierr )
+  call MPI_REDUCE(nbranch, max_nbranch,     1, MPI_INTEGER, MPI_MAX, 0, MPI_COMM_WORLD, ierr )
+  call MPI_REDUCE(nbranch, min_nbranch,     1, MPI_INTEGER, MPI_MIN, 0, MPI_COMM_WORLD, ierr )
   nnodes=nleaf+ntwig
-  call MPI_ALLREDUCE(nleaf, gmax_leaves, 1, MPI_INTEGER, MPI_MAX, MPI_COMM_WORLD, ierr )
-  call MPI_ALLREDUCE(ntwig, gmax_twigs, 1, MPI_INTEGER, MPI_MAX, MPI_COMM_WORLD, ierr )
+  call MPI_REDUCE(nleaf, gmax_leaves, 1, MPI_INTEGER, MPI_MAX, 0, MPI_COMM_WORLD, ierr )
+  call MPI_REDUCE(ntwig, gmax_twigs,  1, MPI_INTEGER, MPI_MAX, 0, MPI_COMM_WORLD, ierr )
 
   call MPI_GATHER(work_local, 1, MPI_REAL, work_loads, 1, MPI_REAL, 0,  MPI_COMM_WORLD, ierr )  ! Gather work integrals
   part_imbal_max = MAXVAL(particles)
@@ -62,20 +64,25 @@ subroutine tree_stats(timestamp)
     open (60,file=cfile)
 
     write(60,'(a20,i7,a22)') 'Tree stats for CPU ', me, ' and global statistics'
-    write(60,'(a50,3i12)') 'new npp, npart, (max): ',npp,npart,nppm
+
+    write(60,'(a50,3i12)') '# procs, walk_threads, max_particles_per_thread', num_pe, num_walk_threads, max_particles_per_thread
+    write(60,'(a50,3i12)') 'nintmax, np_mult, size_tree',nintmax, np_mult,size_tree
+    write(60,'(a50,3i12)') 'npp, npart, nppm(max): ',npp,npart,nppm
+    write(60,'(a50,3i12)') 'N/P',npart/num_pe
     write(60,'(a50,1i12)') 'total # particles: ', total_part
     write(60,'(a50,2e12.4)') 'total/ave work: ', total_work, average_work
     write(60,'(a50,3i12)') 'local # leaves, twigs, keys: ',nleaf_me,ntwig_me,nleaf_me+ntwig_me
     write(60,'(a50,3i12)') 'non-local # leaves, twigs, keys: ',nleaf-nleaf_me,ntwig-ntwig_me,nleaf+ntwig-nleaf_me-ntwig_me
     write(60,'(a50,3i12,f12.1,a6,i12)') 'final # leaves, twigs, keys, (max): ',nleaf,ntwig,nleaf+ntwig, &
               (nleaf+ntwig)/(.01*maxaddress),' % of ',maxaddress
-    write(60,'(a50,3i12)') 'maximum # leaves, twigs, keys: ',maxleaf, maxtwig, maxaddress
+    write(60,'(a50,3i12)') 'maximum # leaves, twigs, keys(=maxaddress): ',maxleaf, maxtwig, maxaddress
     write(60,'(a50,1i12,1f12.1, a6,1i12)') 'Global max # leaves: ',gmax_leaves, gmax_leaves/(.01*maxleaf), ' % of  ', maxleaf
     write(60,'(a50,1i12,1f12.1, a6,1i12)') 'Global max # twigs: ', gmax_twigs, gmax_twigs/(.01*maxtwig), ' % of  ', maxtwig
-    write(60,'(a50,3i12,a3,2i12)') 'Local, max local, global # branches, (max): ', &
-              nbranch,max_nbranch,nbranch_sum,'/',nbranch_local_max,nbranch_max
+    write(60,'(a50,4i12,a3,2i12)') 'Local, max / min local, global # branches, (max): ', &
+              nbranch,max_nbranch,min_nbranch, nbranch_sum,'/',nbranch_local_max,nbranch_max
 
     write (60,'(a50,2i12)') 'Max # multipole fetches/ships per cpu: ',maxval(fetches), maxval(ships)
+    write (60,'(a50,2i12)') 'Min # multipole fetches/ships per cpu: ',minval(fetches), minval(ships)
     write (60,'(a50,2i12)') 'Local #  multipole fetches & ships: ',sum_fetches,sum_ships
     write (60,'(a50,3f12.3)') 'Load imbalance percent,min,max: ',work_imbal,work_imbal_min,work_imbal_max
     write (60,'(a50,f12.3,2i12)') 'Particle imbalance ave,min,max: ',part_imbal,part_imbal_min,part_imbal_max
