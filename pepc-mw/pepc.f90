16c626854bcbf75b858d2b23f8055e4caceedc90
Mathias Winkel <m.winkel@fz-juelich.de>
2011-08-31 09:14:30 +0000

    pepcmw: reduced verbosity level of direct sum

--- pepc-mw/pepc.f90
+++ pepc-mw/pepc.f90
@@ -131,7 +131,7 @@ program pepc
                       num_neighbour_boxes, neighbour_boxes, treediags)
 
      call verifydirect(x, y, z, q, ex, ey, ez, pot, np_local, [1, 2, np_local-1, np_local], &
-                       calc_force_params(eps, force_const, 3), 3, my_rank, n_cpu, MPI_COMM_PEPC)
+                       calc_force_params(eps, force_const, 3), 0, my_rank, n_cpu, MPI_COMM_PEPC)
 
      ! output of tree diagnostics
      if (treediags) then
