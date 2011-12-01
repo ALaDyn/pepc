14a8b8544299300430e2ed647837aec78270d4ab
Robert Speck <r.speck@fz-juelich.de>
2011-12-01 14:03:05 +0000

    initial example for vortex

---
+++ examples/vortex_sphere/vsphere.h
@@ -0,0 +1,26 @@
+&pepcdata
+
+ ispecial = 3
+
+ num_walk_threads = 2
+ mac = 0
+ theta = 0.0
+
+ g = 4.
+ nu = 0.
+
+ rem_freq = 0
+ thresh = 0.
+
+ n_in = 1000
+
+ dt = 0.5
+ ts = 0.
+ te = 10.
+
+ dump_time = 1
+ cp_time = 1
+
+/
+
+
