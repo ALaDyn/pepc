6e1d706d4e11c69255e4c8ddbd114823fa0f734b
Andreas Breslau <a.breslau@fz-juelich.de>
2012-01-04 09:39:55 +0000

    copied sph frontend and backend to grav-sph frontend and backend as basis for the inclusion of gravity.

---
+++ src/frontends/pepc-grav-sph/pepc.f90
@@ -0,0 +1,298 @@
+
+! ==============================================================
+!
+!
+!                  PEPC-SPH
+!
+!    Parallel Efficient Parallel Coulomb-solver: Electrostatics 
+!
+!   $ Revision $
+!
+!   Driver code for Coulomb-solver library lpepc
+!
+!  Major changes:
+!   June 2005: Development begun
+!
+!   See README.compile for summary of program units
+!  ==============================================================
+
+program pepce
+
+  ! TODO: use the openmp module only, when compiling with openmp: !$ use omp_lib
+  ! module_deps has to be changed to remove "!$" when using openmp
+  ! TODO: use omp_lib, only: ...
+  use omp_lib
+
+  use module_walk, only: &
+       num_walk_threads
+
+  use module_interaction_specific, only: &
+       mac_select, force_law
+
+  use module_interaction_specific_types, only: &
+       PARTICLE_TYPE_FIXED
+  
+  use physvars
+! , only: &
+!        trun, &
+!        particles, &
+!        nt, &
+!        npart_total, &
+!        np_local, &
+!        n_cpu, &
+!        my_rank, &
+!        itime, &
+!        ispecial, &
+!        dt, &
+!        idim
+
+  use module_neighbour_test, only: &
+       validate_n_nearest_neighbour_list, &
+       draw_neighbours
+
+  use module_sph, only: &
+       sph, &
+       sph_kernel_tests
+  
+  use module_timings, only: &
+       timer_start, &
+       timer_stop, &
+       timings_LocalOutput, &
+       timings_GatherAndOutput, &
+       t_tot
+       
+  use module_mirror_boxes , only: &
+       neighbour_boxes, &
+       num_neighbour_boxes, &
+       calc_neighbour_boxes, &
+       do_periodic, &
+       constrain_periodic, &
+       periodicity
+
+  use module_pepc, only: &
+       pepc_initialize, &
+       pepc_finalize, &
+       pepc_grow_tree, &
+       pepc_traverse_tree, &
+       pepc_prepare, &
+       pepc_particleresults_clear
+
+
+  use files, only: &
+       openfiles, &
+       closefiles, &
+       write_particles
+
+  use particle_pusher, only: &
+       velocities, &
+       push
+
+  use module_particle_setup, only: &
+       particle_setup
+
+  implicit none
+
+  integer :: omp_thread_num
+  integer :: ierr, ifile, i
+
+
+  ! Allocate array space for tree
+  call pepc_initialize("pepc-sph", my_rank, n_cpu, .true.)
+
+  ! Set up O/P files
+  call openfiles
+
+  ! Time stamp
+  if (my_rank==0) call stamp(6,1)
+  if (my_rank==0) call stamp(15,1)
+
+  ! Each CPU gets copy of initial data
+  call pepc_setup()
+
+
+  ! Set the number of openmp threads.
+  ! Set this only, when compiling with openmp (with !$)
+  ! Set number of openmp threads to the same number as pthreads used in the walk
+  !$ call omp_set_num_threads(num_walk_threads)
+
+  ! Inform the user that openmp is used, and with how many threads
+  !$OMP PARALLEL PRIVATE(omp_thread_num)
+  !$ omp_thread_num = OMP_GET_THREAD_NUM()
+  !$ if( (my_rank .eq. 0) .and. (omp_thread_num .eq. 0) ) write(*,*) 'Using OpenMP with', OMP_GET_NUM_THREADS(), 'threads.'
+  !$OMP END PARALLEL
+
+  ! Set up particles
+  call particle_setup(ispecial)
+
+!  call special_start(ispecial)
+
+  ! initialize calc force params
+  mac_select  = 1 ! NN MAC
+  force_law   = 5 ! NN "Interaction"
+
+  particles(:)%work = 1._8
+
+  call write_particles(itime-1, -0.1)
+
+
+  call pepc_prepare(idim)
+
+  ! TODO: where should do_periodic be set?
+  do_periodic = periodicity(1) .or. periodicity(2) .or. periodicity(3)
+
+  if(my_rank .eq. 0 ) then
+     call sph_kernel_tests(idim)
+  end if
+
+
+
+
+
+
+
+
+
+
+
+
+  ! 
+  if( .not. initialized_v_minus_half ) then
+
+     call pepc_grow_tree(np_local, npart_total, particles)
+
+     mac_select = 1 ! nn-mac
+     force_law = 5  ! neighbour list force law
+     
+     call pepc_particleresults_clear(particles, np_local)
+     
+     call pepc_traverse_tree(np_local, particles)
+
+     call sph(np_local, particles, -1, num_neighbour_boxes, neighbour_boxes, idim)
+
+     ! Integrate velocity for t-dt/2
+     do i = 1, np_local
+        if( btest(particles(i)%data%type, PARTICLE_TYPE_FIXED) ) then
+           ! don't move this particle
+        else
+           particles(i)%data%v_minus_half = particles(i)%data%v - particles(i)%results%sph_force * dt/2.
+           ! this is not perfect, but better than nothing
+        end if
+     end do
+
+
+     ! the temperature is currently not integrated here, knowing that that produces an energy error
+     ! do i = 1, np_local
+     !    particles(i)%data%temperature = particles(i)%data%temperature + particles(i)%results%temperature_change * dt
+     !    if(particles(i)%data%temperature < 0.000000001 ) then
+     !       particles(i)%data%temperature = 0.000000001
+     !    end if
+     ! end do
+
+     ! TODO: calculate work for sph
+     particles(:)%work = 1._8
+
+     
+  end if
+
+
+
+  ! Loop over all timesteps. <= nt to write out the last timestep
+  do while (itime <= nt)
+
+     if (my_rank==0 ) then
+        ifile=6
+           write(ifile,'(//a,i8,(3x,a,f12.3))') &
+                ' Timestep ',itime &
+                ,' total run time = ',trun 
+     endif
+     
+     call timer_start(t_tot)
+     
+     call pepc_grow_tree(np_local, npart_total, particles)
+
+
+     mac_select = 1 ! nn-mac
+     force_law = 5  ! neighbour list force law
+
+     ! TODO: remove this debug output
+     ! write(*,*) 'num_neighbour_boxes:', num_neighbour_boxes
+     ! write(*,*) 'neigbour_boxes:', neighbour_boxes
+
+     call pepc_particleresults_clear(particles, np_local)
+
+     call pepc_traverse_tree(np_local, particles)
+
+     call validate_n_nearest_neighbour_list(np_local, particles, itime, num_neighbour_boxes, neighbour_boxes)
+
+     ! call draw_neighbours(np_local, particles, itime)
+
+     call sph(np_local, particles, itime, num_neighbour_boxes, neighbour_boxes, idim)
+
+
+
+     ! Because the density and force is computed according to the current particle positions and velocities, everything is written out here.
+     call write_particles(itime, trun)
+
+     ! Now the time-step is incremented and the particles are moved.
+     ! The result is not written out again, so the particles_...000000...dat/vtk contains the initial particle configuration with the densities and forces.
+     ! To test the integration based on the computed forces, at least 2 time-steps have to be computed.
+     itime = itime + 1
+     trun  = trun  + dt
+     
+     
+     ! Integrator
+     call velocities(1,np_local,dt)
+     call push(1,np_local,dt)  ! update positions
+
+
+     do i = 1, np_local
+        particles(i)%data%temperature = particles(i)%data%temperature + particles(i)%results%temperature_change * dt
+        if(particles(i)%data%temperature < 0.000000001 ) then
+           particles(i)%data%temperature = 0.000000001
+        end if
+     end do
+
+
+     
+
+     write(*,*) "do_periodic:", do_periodic
+     
+     ! periodic systems demand periodic boundary conditions
+     if (do_periodic) call constrain_periodic(particles(1:np_local)%x(1),particles(1:np_local)%x(2),particles(1:np_local)%x(3),np_local)
+
+
+     
+     ! timings dump
+     call timer_stop(t_tot) ! total loop time without diags
+
+     call timings_LocalOutput(itime)
+     call timings_GatherAndOutput(itime)
+
+
+     ! TODO: calculate work for sph
+     particles(:)%work = 1._8
+
+
+
+  end do
+
+  ! The final particle positions could be written out here, but the densities and forces would belong to the previous timestep.
+  ! This could lead to misinterpretations in the postprocessing and is explicitly NOT done.
+  ! So we knowingly throw away the result of the last time-step.
+  ! To get nt timesteps written out, the time-loop is while itime <= nt
+
+
+  ! deallocate array space for particles
+  call cleanup(my_rank,n_cpu)
+  
+  ! Time stamp
+  if (my_rank==0) call stamp(6,2)
+  if (my_rank==0) call stamp(15,2)
+
+  ! Tidy up O/P files
+  call closefiles
+
+  ! cleanup of lpepc static data
+  call pepc_finalize()
+
+end program pepce
---
+++ src/interaction_specific/grav_sph/module_interaction_specific.f90
@@ -0,0 +1,374 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!>
+!> Encapsulates anything that is directly involved in force calculation
+!> and multipole expansion manipulation
+!> i.e. shifting along the tree, computing forces between particles and cluster, etc.
+!>
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+module module_interaction_specific
+     use module_pepc_types
+     use module_interaction_specific_types
+     implicit none
+     save
+     private
+
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!  public variable declarations  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+      real*8, parameter :: WORKLOAD_PENALTY_MAC  = 1._8 !< TODO: currently unused
+      real*8, parameter :: WORKLOAD_PENALTY_INTERACTION = 30._8
+
+      integer, public :: force_law    = 5      !< 5=NN-list "interaction"
+      integer, public :: mac_select   = 1      !< selector for multipole acceptance criterion, 1 = NN-MAC
+
+      namelist /calc_force_nearestneighbour/ force_law, mac_select
+
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!  public subroutine declarations  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+      ! currently, all public functions in module_interaction_specific are obligatory
+      public multipole_from_particle
+      public shift_multipoles_up
+      public results_add
+      public calc_force_per_interaction
+      public calc_force_per_particle
+      public mac
+      public particleresults_clear
+      public calc_force_init
+      public calc_force_finalize
+      public calc_force_prepare
+      public get_number_of_interactions_per_particle
+
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!  private variable declarations  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!  subroutine-implementation  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      contains
+
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !>
+      !> Computes multipole properties of a single particle
+      !>
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      subroutine multipole_from_particle(particle_pos, particle, multipole)
+        implicit none
+        real*8, intent(in) :: particle_pos(3)
+        type(t_particle_data), intent(in) :: particle
+        type(t_tree_node_interaction_data), intent(out) :: multipole
+
+        ! use velocity (v) at same time step as coordinate, not v_minus_half
+        multipole = t_tree_node_interaction_data(particle_pos,particle%q, particle%v, particle%temperature, -13._8, -13._8 )
+        ! set rho to -13 as dummy.
+        ! TODO: find a better place to store rho
+
+      end subroutine
+
+
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !>
+      !> Accumulates multipole properties of child nodes to parent node
+      !>
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      subroutine shift_multipoles_up(parent, children)
+        implicit none
+        type(t_tree_node_interaction_data), intent(out) :: parent
+        type(t_tree_node_interaction_data), intent(in) :: children(:)
+
+        integer :: nchild, i
+
+        nchild = size(children)
+
+        parent%coc = [0._8, 0._8, 0._8]
+        parent%q = 0._8
+
+        do i=1,nchild
+          parent%coc = parent%coc + children(i)%coc
+          parent%q   = parent%q   + children(i)%q
+        end do
+
+        parent%coc = parent%coc / nchild
+
+        ! set velocity, temperature and rho for tree node to a nonsense value which may be recognised as nonsense when one tries to use them
+        parent%v = [-13._8,-13._8,-13._8]
+        parent%temperature = -13._8
+        parent%rho = -13._8
+        parent%h = -13._8
+
+      end subroutine
+
+
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !>
+      !> adds res2 to res1
+      !>
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      subroutine results_add(res1, res2)
+        implicit none
+        type(t_particle_results), intent(inout) :: res1
+        type(t_particle_results), intent(in) :: res2
+
+        ! TODONN: ist das wirklich alles?
+        res1 = res2
+
+      end subroutine
+
+
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !>
+      !> initializes interaction specific parameters, redas them from file
+      !> if optional argument para_file_name is given
+      !>
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      subroutine calc_force_init(para_file_available, para_file_name, my_rank)
+        implicit none
+        logical, intent(in) :: para_file_available
+        character(*), intent(in) :: para_file_name
+        integer, intent(in) :: my_rank
+        integer, parameter :: para_file_id = 47
+
+        if (para_file_available) then
+            open(para_file_id,file=para_file_name)
+
+            if(my_rank .eq. 0) write(*,*) "reading parameter file, section calc_force_nearestneighbour: ", para_file_name
+            read(para_file_id,NML=calc_force_nearestneighbour)
+
+            close(para_file_id)
+        endif
+
+      end subroutine
+
+
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !>
+      !> computes derived parameters for calc force module
+      !>
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      subroutine calc_force_prepare()
+        implicit none
+        ! nothing to do here
+      end subroutine
+
+
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !>
+      !> subroutine must return the estimated number of iteractions per particle
+      !> for the current mac and/or parameters and the supplied total number of particles
+      !>
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      subroutine get_number_of_interactions_per_particle(npart_total, nintmax)
+        implicit none
+        integer, intent(in) :: npart_total !< total number of particles
+        integer, intent(out) :: nintmax !< maximum number of interactions per particle
+
+        real*8 :: invnintmax !< inverse of nintmax to avoid division by zero for theta == 0.0
+
+        real*8, parameter :: theta2 = 0.3**2 !TODO: this function must be adapted to the actual MAC
+
+        ! Estimate of interaction list length - Hernquist expression
+        ! applies for BH-MAC
+        invnintmax = max(theta2 / (35.*log(1.*npart_total)) , 1._8/npart_total)
+        nintmax    = int(1._8/invnintmax)
+
+      end subroutine
+
+
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !>
+      !> finalizes the calc force module at end of simulation
+      !>
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      subroutine calc_force_finalize()
+        implicit none
+        ! nothing to do here
+      end subroutine calc_force_finalize
+
+
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !>
+      !> generic Multipole Acceptance Criterion
+      !>
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      function mac(particle, node, dist2, boxlength2)
+        implicit none
+
+        logical :: mac
+        integer, intent(in) :: node
+        type(t_particle), intent(in) :: particle
+        real*8, intent(in) :: dist2
+        real*8, intent(in) :: boxlength2
+
+        select case (mac_select)
+            case (0)
+              ! Barnes-Hut-MAC
+              ! mac = (theta2 * dist2 > boxlength2)
+            case (1)
+              ! NN-MAC: we may "interact" with the node if it is further away than maxdist2 --> this leads to the node *not* being put onto the NN-list (strange, i know)
+              ! first line: original formulation, last line: after transition to formulation with only one square root
+              ! mac = sqrt(dist2) - sqrt(3.* boxlength2)  >  sqrt(results%maxdist2)                                ! + sqrt(3.*boxlength2)
+              !     = sqrt(dist2)                         >  sqrt(results%maxdist2) + sqrt(3.*boxlength2)          ! ^2
+              !     =      dist2                          > (sqrt(results%maxdist2) + sqrt(3.*boxlength2))**2
+              !     =      dist2                          > results%maxdist2 + 2.*sqrt( 3.*results%maxdist2*boxlength2) + 3.*boxlength2
+                mac =      dist2                          > particle%results%maxdist2 +    sqrt(12.*particle%results%maxdist2*boxlength2) + 3.*boxlength2
+              ! TODO NN: this estimation should be evaluated without (!!) any square roots for performance reasons (which does not seem to be trivial)
+            case default
+              ! N^2 code
+              mac = .false.
+        end select
+
+      end function
+
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !>
+      !> clears result in t_particle datatype - usually, this function does not need to be touched
+      !> due to dependency on module_pepc_types and(!) on module_interaction_specific, the
+      !> function cannot reside in module_interaction_specific that may not include module_pepc_types
+      !>
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      subroutine particleresults_clear(particles, nparticles)
+        use module_pepc_types
+        use module_htable
+        use treevars
+        use module_spacefilling
+        implicit none
+        type(t_particle), intent(inout) :: particles(nparticles)
+        integer, intent(in) :: nparticles
+
+        integer*8 :: key
+        integer :: addr, i
+        real*8, dimension(:), allocatable :: boxdiag2
+
+        allocate(boxdiag2(0:nlev))
+        boxdiag2(0) = (sqrt(3.)*boxsize)**2
+        do i=1,nlev
+           boxdiag2(i) =  boxdiag2(i-1)/4.
+        end do
+
+
+        ! for each particle, we traverse the tree upwards, until the current twig
+        ! contains more leaves than number of necessary neighbours - as a first guess for the
+        ! search radius, we use its diameter
+        do i=1,nparticles
+            key = particles(i)%key
+
+            particles(i)%results%maxdist2 = huge(0._8)
+            particles(i)%results%neighbour_nodes(:) = 0
+            particles(i)%results%maxidx             = 1
+
+            do while (key .ne. 0)
+              if (testaddr(key, addr)) then
+                if (htable(addr)%leaves > num_neighbour_particles) then
+                  ! this twig contains enough particles --> we use its diameter as search radius
+                  particles(i)%results%maxdist2 = boxdiag2(level_from_key(key))
+                  particles(i)%results%neighbour_nodes(1:num_neighbour_particles) = htable(addr)%node
+
+                  exit ! from this loop
+                endif
+              endif
+
+              key = ishft(key, -3)
+              if (key.eq.0) then
+               write(*,*) particles(i)
+              endif
+            end do
+
+            particles(i)%results%dist2(1:num_neighbour_particles) = particles(i)%results%maxdist2
+            particles(i)%results%dist_vector(:,1:num_neighbour_particles) = -13._8 
+        end do
+
+
+      end subroutine
+
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        !>
+        !> Force calculation wrapper.
+        !> This function is thought for pre- and postprocessing of
+        !> calculated fields, and for being able to call several
+        !> (different) force calculation routines
+        !>
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        subroutine calc_force_per_interaction(particle, inode, delta, dist2, vbox)
+          use module_interaction_specific_types
+          use treevars
+          implicit none
+
+          integer, intent(in) :: inode
+          type(t_particle), intent(inout) :: particle
+          real*8, intent(in) :: vbox(3), delta(3), dist2
+
+          select case (force_law)
+            case (5)
+                call update_nn_list(particle, inode, delta, dist2)
+                particle%work = particle%work + WORKLOAD_PENALTY_INTERACTION
+          end select
+
+
+        end subroutine calc_force_per_interaction
+
+
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        !>
+        !> Force calculation wrapper for contributions that only have
+        !> to be added once per particle
+        !>
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        subroutine calc_force_per_particle(particles, nparticles)
+          use module_interaction_specific_types
+          use treevars, only : me
+          implicit none
+
+          integer, intent(in) :: nparticles
+          type(t_particle), intent(inout) :: particles(:)
+
+          ! currently nothing to do here
+
+        end subroutine calc_force_per_particle
+
+
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        !>
+        !>
+        !>
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        subroutine update_nn_list(particle, inode, d, dist2)
+          use module_pepc_types
+          use treevars
+          implicit none
+          include 'mpif.h'
+
+          integer, intent(in) :: inode !< index of particle to interact with
+          real*8, intent(in) :: d(3), dist2 !< separation vector and magnitude**2 precomputed in walk_single_particle
+          type(t_particle), intent(inout) :: particle
+
+          integer :: ierr, tmp(1)
+
+          if (dist2 < particle%results%maxdist2) then
+            ! add node to NN_list
+            particle%results%neighbour_nodes(particle%results%maxidx) = inode
+            particle%results%dist2(particle%results%maxidx)           = dist2
+            particle%results%dist_vector(:,particle%results%maxidx) = d
+            tmp                       = maxloc(particle%results%dist2(1:num_neighbour_particles)) ! this is really ugly, but maxloc returns a 1-by-1 vector instead of the expected scalar
+            particle%results%maxidx   = tmp(1)
+            particle%results%maxdist2 = particle%results%dist2(particle%results%maxidx)
+          else
+            ! node is further away than farest particle in nn-list --> can be ignored
+          endif
+
+        end subroutine update_nn_list
+
+        
+  end module module_interaction_specific
