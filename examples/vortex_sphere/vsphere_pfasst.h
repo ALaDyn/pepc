b6752ec13c319d906564dbcde8e792758dcb1081
Robert Speck <r.speck@fz-juelich.de>
2012-03-21 12:20:13 +0000

    added .h file for debugging

--- examples/vortex_sphere/vsphere_pfasst.h
+++ examples/vortex_sphere/vsphere_pfasst.h
@@ -11,20 +11,20 @@
  thresh = 0.0D-04
  m_h = 0.1
 
- n_in = 2000
+ n_in = 10001
 
  dt = 0.5
  ts = 0.
- te = 50.
+ te = 16
 
- dump_time = 1
+ dump_time = 0
  cp_time = 0
 /
 
 &calc_force_vortex
   force_law  = 62
-  mac_select = 1
-  theta2     = 0.09
+  mac_select = 0
+  theta2     = 0.2401
 /
 
 &libpepc
@@ -34,7 +34,7 @@
  /
 
 &walk_para_pthreads
- num_walk_threads         = 4
+ num_walk_threads         = 16
  max_particles_per_thread = 2000
 /
 
@@ -46,6 +46,9 @@
 &pfasst
 
  num_space_instances = 1
+ num_coarse_sweeps = 1
  niter = 4
+ NnodesF = 3
+ NnodesG = 2
 
 /
