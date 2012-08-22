8ff834a798cafe73b82bc0b480c17c12bde2c298
Christian Salmagne <c.salmagne@fz-juelich.de>
2012-08-22 11:28:28 +0000

    Added subroutine read_particles_mpiio_from_filename to checkpoint module (used in pepc-numpy)

--- src/utils/module_checkpoint.f90
+++ src/utils/module_checkpoint.f90
@@ -50,6 +50,7 @@ module module_checkpoint
       public write_particles_binary
       public write_particles_mpiio
       public read_particles_mpiio
+      public read_particles_mpiio_from_filename
 
       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -269,6 +270,61 @@ module module_checkpoint
 
             filename = trim(filename)//".h"
             open(filehandle, file=trim(filename),action='read')
+            call pepc_read_parameters(filehandle)
+            close(filehandle)
+
+          end subroutine
+
+          !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          !>
+          !>
+          !>
+          !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          subroutine read_particles_mpiio_from_filename(comm, my_rank, n_cpu, itime, np_local, n_total, dp, filename)
+            use module_pepc_types
+            use module_debug
+            use module_pepc, only : pepc_read_parameters
+            implicit none
+            integer, intent(in) :: my_rank, n_cpu
+            integer, intent(out) :: itime, np_local
+            integer*8, intent(out) :: n_total
+            integer, intent(in) :: comm
+            character(*), intent(in) :: filename
+            character(255) :: filename2
+            type(t_particle), allocatable :: dp(:)
+            integer :: fh, ierr, status(MPI_STATUS_SIZE)
+            integer*8 :: remain
+
+
+            call pepc_status("READ PARTICLES MPI: "//filename)
+
+            call MPI_FILE_OPEN(comm,trim(filename),MPI_MODE_RDONLY,MPI_INFO_NULL,fh,ierr)
+
+            if (ierr .ne. MPI_SUCCESS) then
+              DEBUG_ERROR(*,'read_particles_mpiio_from_file(): file open failed', my_rank, ierr, filename)
+            end if
+
+            ! Set file view to BYTE for header, only rank 0 writes it
+            call MPI_FILE_SET_VIEW(fh, 0_MPI_OFFSET_KIND, MPI_BYTE, MPI_BYTE, 'native', MPI_INFO_NULL, ierr)
+            call MPI_FILE_READ(fh, n_total, 1, MPI_INTEGER8, status, ierr) ! # particles
+            call MPI_FILE_READ(fh, itime,   1, MPI_INTEGER,  status, ierr) ! Last successful timestep (new ts)
+
+            np_local = int(n_total/n_cpu, kind(np_local))
+            remain = n_total-np_local*n_cpu
+            if ((remain > 0) .and. (my_rank < remain)) np_local = np_local+1
+
+            if (allocated(dp)) deallocate(dp)
+            allocate(dp(1:np_local))
+
+            ! Redefine file view, now with our custom type
+            call MPI_FILE_SET_VIEW(fh, max_header_size, MPI_TYPE_particle, MPI_TYPE_particle, 'native', MPI_INFO_NULL, ierr)
+            ! Read particle data
+            call MPI_FILE_READ_ORDERED(fh, dp, np_local, MPI_TYPE_particle, status, ierr)
+            ! Close file
+            call MPI_FILE_CLOSE(fh,ierr)
+
+            filename2 = trim(filename)//".h"
+            open(filehandle, file=trim(filename2),action='read')
             call pepc_read_parameters(filehandle)
             close(filehandle)
 
