f83723ad211741c5f4377cee08325a9cc936b0e7
Mathias Winkel <m.winkel@fz-juelich.de>
2013-07-15 20:50:31 +0000

    added missing files from prev. commit

---
+++ src/frontends/pepc-andreev/module_globals.f90
@@ -0,0 +1,36 @@
+! This file is part of PEPC - The Pretty Efficient Parallel Coulomb Solver.
+! 
+! Copyright (C) 2002-2012 Juelich Supercomputing Centre, 
+!                         Forschungszentrum Juelich GmbH,
+!                         Germany
+! 
+! PEPC is free software: you can redistribute it and/or modify
+! it under the terms of the GNU Lesser General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+! 
+! PEPC is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU Lesser General Public License for more details.
+! 
+! You should have received a copy of the GNU Lesser General Public License
+! along with PEPC.  If not, see <http://www.gnu.org/licenses/>.
+!
+
+!>
+!> global declarations for pepc-andreev
+!>
+module pepca_globals
+  use module_pepc_types
+  implicit none
+  save
+  
+  integer(kind_dim), parameter :: dim = 2
+  
+  ! grid for density output
+  integer, parameter :: Ngrid(3) = [500, 1000, 1]
+
+  logical :: root
+
+end module
