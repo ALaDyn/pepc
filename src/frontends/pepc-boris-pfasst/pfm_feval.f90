bcc0a7a20f9727074592ea1270b3c0221ca51834
Robert Speck <r.speck@fz-juelich.de>
2013-10-31 10:05:05 +0000

    pepc-boris-pfasst: implemented boris solver in verlet sweeper.. this version does compile, hooray

--- src/frontends/pepc-boris-pfasst/pfm_feval.f90
+++ src/frontends/pepc-boris-pfasst/pfm_feval.f90
@@ -154,7 +154,7 @@ module pfm_feval
       type(level_params_t), pointer :: levelctx
       integer(kind_particle) :: i
 
-      call pepc_status('|------> buld_rhs()')
+      call pepc_status('|------> build_rhs()')
       call ptr_print('rhs', rhsptr)
       call ptr_print('E', Eptr)
       call ptr_print('Q', Qptr)
@@ -168,6 +168,9 @@ module pfm_feval
       DEBUG_ASSERT(E%params%nparts==rhs%params%nparts)
       DEBUG_ASSERT(E%params%dim==rhs%params%dim)
 
+      allocate(particles(E%params%nparts))
+      call encap_to_particles(particles, Eptr, ctx)
+
       B0 = [0.0_8, 0.0_8, pepcboris_nml%setup_params(PARAMS_B0)]
 
       do i=1,size(particles, kind=kind(i))
@@ -178,19 +181,69 @@ module pfm_feval
 
       deallocate(particles)
 
-
     end subroutine
 
     !> solve for the updated velocity, e.g. using the Boris solver
-    subroutine impl_solver(v, level, ctx, v_old, E_old, E_new, SDCint, dt)
+    subroutine impl_solver(vptr, level, ctx, v_oldptr, E_oldptr, E_newptr, SDCintptr, dt)
       use module_pepc
       use iso_c_binding, only : c_ptr, c_int, c_f_pointer
       use module_debug, only : dbg, DBG_STATS, pepc_status
+      use pepcboris_helper
       implicit none
-      type(c_ptr),    intent(in), value :: v, v_old, E_old, E_new, SDCint, ctx
+      type(c_ptr),    intent(in), value :: vptr, v_oldptr, E_oldptr, E_newptr, SDCintptr, ctx
       integer(c_int), intent(in)        :: level
       real(pfdp),     intent(in)        :: dt
-      ! FIXME
+
+      real*8               :: beta, gam
+      real*8, dimension(3) :: B0, uminus, uprime, uplus, t, s, Emean
+
+      type(t_particle), allocatable :: particles(:)
+      type(app_data_t), pointer :: v, v_old, E_new, E_old, SDCint
+      type(level_params_t), pointer :: levelctx
+      integer(kind_particle) :: i
+
+      call pepc_status('|------> impl_solver()')
+      call ptr_print('v', vptr)
+      call ptr_print('v_old', v_oldptr)
+      call ptr_print('E_new', E_newptr)
+      call ptr_print('E_old', E_oldptr)
+      call ptr_print('SDCint', SDCintptr)
+      call ptr_print('ctx', ctx)
+
+      call c_f_pointer(vptr, v)
+      call c_f_pointer(v_oldptr, v_old)
+      call c_f_pointer(E_newptr, E_new)
+      call c_f_pointer(E_oldptr, E_old)
+      call c_f_pointer(SDCintptr, SDCint)
+      call c_f_pointer(ctx, levelctx)
+
+      B0 = [0.0_8, 0.0_8, pepcboris_nml%setup_params(PARAMS_B0)]
+
+      allocate(particles(E_new%params%nparts))
+      call encap_to_particles(particles, E_newptr, ctx)
+
+      do i = 1,size(particles, kind=kind(i))
+
+         ! charge/mass*time-constant
+        beta   = particles(i)%data%q / (2._8 * particles(i)%data%m) * dt
+        Emean(:)  = (E_old%v(1:3,i) + E_new%v(1:3,i)) / 2._8
+        ! first half step with electric field
+        uminus(:) = v_old%v(1:3,i) + dt / 2._8 * ( particles(i)%data%q / particles(i)%data%m * Emean(:) + SDCint%v(1:3,i) )
+        ! gamma factor
+        !gam    = sqrt( 1._8 + ( dot_product(uminus, uminus) ) / unit_c2 )
+        gam    = 1._8
+        ! rotation with magnetic field
+        t      = beta/gam * B0
+        uprime = uminus + cross_product(uminus, t)
+        s      = 2._8 * t / (1._8 + dot_product(t, t))
+        uplus  = uminus + cross_product(uprime, s)
+        ! second half step with electric field
+        v%v(1:3,i) = uplus(:) + dt / 2._8 * ( particles(i)%data%q / particles(i)%data%m * Emean(:) + SDCint%v(1:3,i) )
+
+      end do
+
+      deallocate(particles)
+
     end subroutine
 
 
