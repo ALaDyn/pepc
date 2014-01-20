1269f16f67f17adeb57305609d4ffae4b607154f
Benedikt Steinbusch <b.steinbusch@fz-juelich.de>
2014-01-20 15:36:22 +0000

    Adapt analysis script to classical set up.

--- examples/kh_2d/find_growth_rate.py
+++ examples/kh_2d/find_growth_rate.py
@@ -1,63 +1,75 @@
 #!/usr/bin/env python
 
 import sys
+import glob
+import multiprocessing as mp
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import t as student_tau
 
 import fieldblob as fb
 from fit import lsfit
 
 expmodel = lambda x, p: p[0] * np.exp(x * p[1]) + p[2]**2
+linmodel = lambda x, p: np.log(p[0]) + x * p[1]
+analytic = lambda x: 0.2 - 0.2 * (2.0 * (x - 0.5))**2
+
+def ky_of_fieldblob(fn):
+  ly = fb.extenty_of_fieldblob(fn)
+  ny = fb.ny_of_fieldblob(fn)
+  return 2 * np.pi * np.fft.fftfreq(ny, ly / ny)
 
 def spectrum_of_fieldblob(fname):
   y  = fb.field_of_fieldblob(fname)
-  yc = y[256 - 25: 256 + 25]
-  
-  return np.mean(np.abs(np.fft.rfft(yc, axis = 1))**2, axis = 0)
+  yc = y[127:128]
+
+  #return np.mean(np.abs(np.fft.rfft(yc, axis = 1))**2, axis = 0)
+  return np.mean(np.abs(np.fft.rfft(yc, axis = 1)), axis = 0)
 
 if __name__ == '__main__':
-  if (len(sys.argv) < 4): sys.exit('syntax: find_growth_rate.py ntstart ntend step')
-  ntstart = int(sys.argv[1])
-  ntend   = int(sys.argv[2])
-  step    = int(sys.argv[3])
+  if (len(sys.argv) < 3): sys.exit('syntax: find_growth_rate.py { potential | ey | ... } nmodes [tstart tend]')
+  field = sys.argv[1]
+  nmodes = int(sys.argv[2])
+  files = glob.glob('fields/' + field + '_??????.bin')
+  files.sort()
 
-  nts = range(ntstart, ntend + 1, step)
-  files = [ 'fields/ey_{0:06d}.bin'.format(nt) for nt in nts ]
-  spectra = [ spectrum_of_fieldblob(f) for f in files ]
-  spectra = np.array(spectra)
-  t = np.fromiter(( fb.t_of_fieldblob(f) for f in files ),
-    dtype = np.float64, count = len(nts))
+  pool = mp.Pool()
 
-  ly = fb.extenty_of_fieldblob(files[0])
-  ny = fb.ny_of_fieldblob(files[0])
-  k  = 2 * np.pi * np.fft.fftfreq(ny, ly / ny)
+  t = np.array(pool.map(fb.t_of_fieldblob, files))
+  (tstart, tend) = (np.float(sys.argv[3]), np.float(sys.argv[4])) if (len(sys.argv) > 3) else (t[0], t[-1])
 
-  i0 = np.unravel_index(np.argmax(spectra), spectra.shape)[1]
-  k0 = k[i0]
-  dk = k[i0 + 1] - k[i0]
-  y = spectra[:, i0]
-  p = lsfit(t, y, expmodel, [ 1.0, 0.01, 0.0 ], maxfev = 10000)[0]
-  gamma0 = p[1]
-  print 'i0:    ', i0
-  print 'k0:    ', k0, '+-', dk
-  print 'gamma: ', p[1]
-  plt.figure()
-  plt.semilogy(t, y)
-  plt.semilogy(t, expmodel(t, p))
-  
-  gamma = np.zeros(32)
+  # TODO: read from configuration
+  B0 = 2.0
+  M  = 16.0
+  T  = 1.0
+  Dx = np.sqrt(M * T) / B0
+  V0 = Dx * B0 / M
+
+  k = Dx * ky_of_fieldblob(files[0])
+  spectra = np.array(pool.map(spectrum_of_fieldblob, files)).transpose()
+
+  mask = np.where(np.logical_and(t >= tstart, t <= tend))
+  tfit = t[mask]
+
+  CONFIDENCE_LEVEL = 0.95
+  #confidence_scale = student_tau.interval(CONFIDENCE_LEVEL, len(tfit), loc=0, scale=1.0)[1]
+  confidence_scale = 1.95
+
+  gamma = np.zeros(nmodes + 1)
+  stddev = np.zeros_like(gamma)
   res = []
-  for i in range(1,32):
+  for i in range(1, nmodes + 1):
+    plt.figure()
+    plt.title("mode number: " + str(i))
+    plt.semilogy(t, spectra[i, :])
     try:
-      p = lsfit(t, spectra[:, i], expmodel, [ 1.0, 0.01, 0.0 ], maxfev = 10000)[0]
-      if ((spectra[-1, i] - spectra[0, i]) > 100.0 * spectra[0, i]):
+      (p, covariance) = lsfit(tfit, np.log(spectra[i][mask]), linmodel, [ 1.0, 0.01 ], maxfev = 10000)
+      plt.semilogy(tfit, np.exp(linmodel(tfit, p)))
+      if True: # ((spectra[-1, i] - spectra[0, i]) > 1.0 * spectra[0, i]):
         print 'p[', i, ']: ', p
-        gamma[i] = p[1]
-        plt.figure()
-        plt.title(str(i))
-        plt.semilogy(t, spectra[:, i])
-        plt.semilogy(t, expmodel(t, p))
+        gamma[i] = Dx / V0 * p[1]
+        stddev[i] = Dx / V0 * np.sqrt(covariance[1,1])
         res.append((k[i], gamma[i]))
       else:
         print 'p[', i, ']: ', p, ' rejected!'
@@ -66,9 +78,10 @@ if __name__ == '__main__':
       gamma[i] = 0
 
   plt.figure()
-  plt.semilogy(k[1:32], gamma[1:32], 'k.')
+  plt.plot(np.linspace(0,1,100), analytic(np.linspace(0,1,100)), 'k-')
+  plt.errorbar(k[1:len(gamma)], gamma[1:], yerr = confidence_scale * stddev[1:], fmt = 'k.')
 
   print '{', ', '.join(['{{{0}, {1}}}'.format(e[0], e[1]) for e in res]), '}'
-  
+
   plt.show()
 
