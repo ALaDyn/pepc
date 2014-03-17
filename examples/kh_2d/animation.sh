d26753b31890cb007b9b0dfe016b46a4d098e0c8
Mathias Winkel <m.winkel@fz-juelich.de>
2014-03-17 14:50:33 +0000

    examples/kh_2d: animation.sh now even works :-)

--- examples/kh_2d/animation.sh
+++ examples/kh_2d/animation.sh
@@ -2,64 +2,60 @@
 
 if [ "$#" -lt 1 ]; then
     echo "Produces a video from fields/heatmap_XXX_??????.bin.png"
-    echo "Usage: ${0} ne|ni|nefromleft|nifromleft|potential|ex|ey|vex|vey|vix|viy"
+    echo "Usage: ${0} [FIELD [FIELD [FIELD [..]]]]"
+    echo "   with FIELD=ne|ni|nefromleft|nifromleft|potential|ex|ey|vex|vey|vix|viy"
+    echo "If more than one field is supplied, in addition to the"
+    echo "separate videos, another video is generated that shows"
+    echo "all individual movies next to each other."
     exit 1
 fi
 
 QUALITY=6
-FILEOUT="./heatmap_${1}.bin"
 
-rm -f animation.list*
-
-FFMPEGFILES="concat:"
-FFMPEGOUT="./heatmap_"
-for datafield in "$@"
-do
-  FILENAME="animation.list.${datafield}"
-  ls -1 fields/heatmap_${datafield}_??????.bin.png | tee animation.list | sed "s/\(.*\)/file '\1'/" > ${FILENAME}
-  FFMPEGFILES="${FFMPEGFILES}${FILENAME}|"
-  FFMPEGOUT="${FFMPEGOUT}_${datafield}"
-done
-
-# see http://www.mplayerhq.hu/DOCS/HTML/de/menc-feat-enc-libavcodec.html
+OVFILES_FFMPEG=""
+OVDATA_FFMPEG=""
+IDX="0"
+FILEMERGED=""
 
 for datafield in "$@"
 do
+    echo "################### processing ${datafield} ####################"
+    FILENAME_FFMPEG="animation.ffmpeg"
+    FILENAME_MPLAYER="animation.mencoder"
+    FILEOUT="./heatmap_${datafield}"
+    FILEMERGED="${FILEMERGED}_${datafield}"
+    OVFILES_FFMPEG="${OVFILES_FFMPEG} -i ${FILEOUT}.mkv"
+    if [ "${IDX}" -eq 0 ]; then
+      OVDATA_FFMPEG="[0:v]pad=iw*$#:ih"
+    else
+      PREVIDX=$((${IDX} - 1))
+      OVDATA_FFMPEG="${OVDATA_FFMPEG}[vid${PREVIDX}];[vid${PREVIDX}][${IDX}:v]overlay=w*${IDX}"
+    fi
+    IDX=$((${IDX} + 1))
+    ls -1 fields/heatmap_${datafield}_??????.bin.png | tee ${FILENAME_MPLAYER} | sed "s/\(.*\)/file '\1'/" > ${FILENAME_FFMPEG}
+
     case $QUALITY in
-	0) # even higher: changed bitrate from 800k (approx default) to 3000kb/s
-	    type mencoder || { echo >&2 "I require mencoder but it's not installed.  Aborting. Use JuropaGPFS nodes."; exit 1; }
-	    mencoder mf://@animation.list -mf fps=16:type=png -ovc lavc -lavcopts vcodec=mpeg4:vbitrate=3000:mbd=2:mv0:trell:v4mv:cbp:last_pred=3:predia=2:dia=2:vmax_b_frames=2:vb_strategy=1:precmp=2:cmp=2:subcmp=2:preme=2:qns=2 -oac copy -o "$FILEOUT.avi"
-	    ;;
-	1) # highest
-	    type mencoder || { echo >&2 "I require mencoder but it's not installed.  Aborting. Use JuropaGPFS nodes."; exit 1; }
-	    mencoder mf://@animation.list -mf fps=16:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:mv0:trell:v4mv:cbp:last_pred=3:predia=2:dia=2:vmax_b_frames=2:vb_strategy=1:precmp=2:cmp=2:subcmp=2:preme=2:qns=2 -oac copy -o "$FILEOUT.avi"
-	    ;;
-	2)
-	    type mencoder || { echo >&2 "I require mencoder but it's not installed.  Aborting. Use JuropaGPFS nodes."; exit 1; }
-	    mencoder mf://@animation.list -mf fps=16:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell:v4mv:last_pred=2:dia=-1:vmax_b_frames=2:vb_strategy=1:cmp=3:subcmp=3:precmp=0:vqcomp=0.6:turbo -oac copy -o "$FILEOUT.avi"
-	    ;;
-	3)
-	    type mencoder || { echo >&2 "I require mencoder but it's not installed.  Aborting. Use JuropaGPFS nodes."; exit 1; }
-	    mencoder mf://@animation.list -mf fps=16:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell:v4mv:turbo -oac copy -o "$FILEOUT.avi"
-	    ;;
-	4) #@ lowest
-	    type mencoder || { echo >&2 "I require mencoder but it's not installed.  Aborting. Use JuropaGPFS nodes."; exit 1; }
-	    mencoder mf://@animation.list -mf fps=16:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:turbo -oac copy -o "$FILEOUT.avi"
-	    ;;
-	5) # lossless h.264
-	    type ffmpeg || { echo >&2 "I require ffmpeg but it's not installed.  Aborting. Use your workstation computer."; exit 1; }
-	    ffmpeg -f concat -i "${FFMPEGFILES}" -r 25 -c:v libx264 -preset veryslow -qp 0 "${FFMPEGOUT}.mkv"
-           #compression# = { ultrafast | veryslow }
-	    ;;
-	6) # high-quality h.264
-	    type ffmpeg || { echo >&2 "I require ffmpeg but it's not installed.  Aborting. Use your workstation computer."; exit 1; }
-	    ffmpeg -f concat -i "${FFMPEGFILES}" -r 25 -c:v libx264 "${FFMPEGOUT}.mkv"
+        5) # lossless h.264
+            type ffmpeg || { echo >&2 "I require ffmpeg but it's not installed.  Aborting. Use your workstation computer."; exit 1; }
+            ffmpeg -f concat -i "${FILENAME_FFMPE}" -r 25 -c:v libx264 -preset veryslow -qp 0 "${FILEOUT}.mkv"
             #compression# = { ultrafast | veryslow }
-	    ;;
-	7) # high-quality, half size h.264
-	    type ffmpeg || { echo >&2 "I require ffmpeg but it's not installed.  Aborting. Use your workstation computer."; exit 1; }
-	    ffmpeg -f concat -i "${FFMPEGFILES}" -r 25 -c:v libx264 -preset veryslow -crf 18 -vf scale=iw/2:-1 -sws_flags bilinear "${FFMPEGOUT}.mkv"
+            ;;
+        6) # high-quality h.264
+            type ffmpeg || { echo >&2 "I require ffmpeg but it's not installed.  Aborting. Use your workstation computer."; exit 1; }
+            ffmpeg -f concat -i "${FILENAME_FFMPEG}" -r 25 -c:v libx264 "${FILEOUT}.mkv"
             #compression# = { ultrafast | veryslow }
-	    ;;
+            ;;
+        7) # high-quality, half size h.264
+            type ffmpeg || { echo >&2 "I require ffmpeg but it's not installed.  Aborting. Use your workstation computer."; exit 1; }
+            ffmpeg -f concat -i "${FILENAME_FFMPEG}" -r 25 -c:v libx264 -preset veryslow -crf 18 -vf scale=iw/2:-1 -sws_flags bilinear "${FILEOUT}.mkv"
+            #compression# = { ultrafast | veryslow }
+            ;;
     esac
 done
+
+if [ "$#" -gt 1 ]; then
+  echo "################### merging videos ####################"
+  echo ffmpeg ${OVFILES_FFMPEG} -filter_complex "${OVDATA_FFMPEG}" -r 25 -c:v libx264 "heatmap${FILEMERGED}.mkv"
+  ffmpeg ${OVFILES_FFMPEG} -filter_complex "${OVDATA_FFMPEG}" -r 25 -c:v libx264 "heatmap${FILEMERGED}.mkv"
+fi
+
