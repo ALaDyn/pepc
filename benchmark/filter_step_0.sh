b34e7384f9928bb1892b22f810c0de895877b7dd
Dirk Brömmel <d.broemmel@fz-juelich.de>
2019-02-14 14:48:11 +0000

    Updates from trunk and branches/benchmarking

---
+++ benchmark/filter_step_0.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+awk '
+ BEGIN { prtln = 1; fl = 1;}
+ /.*computing step\s*:\s*0\s*/ { print "FOUND STEP 0, SKIPPING"; prtln = 0; }
+ /.*computing step\s*:\s*1\s*/ { print "FOUND STEP 1, ECHOING"; prtln = 1; }
+ {if ( prtln == 1 ) { print $0; }}
+' $1 > ${1}.filtered
