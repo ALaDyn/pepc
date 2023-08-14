58810aede6db311eb54341f4a8aaef9b399fee8c
Dirk Brömmel <d.broemmel@fz-juelich.de>
2023-08-14 15:40:49 +0200

    Splitting vortex backend into different types
    To preseve the behaviour (and runtime) of `pepc-v` as it was, add a
    backend type for `pepc-dvh` that contains the additional observable. The
    type can be selected at compile time. In the long-run, consider to make
    this a runtime option to compute the observable on-demand.
    This helps !3.

--- src/interaction_specific/vortex/module_interaction_specific_types_PSI.f90
+++ src/interaction_specific/vortex/module_interaction_specific_types_PSI.f90
@@ -40,11 +40,12 @@ module module_interaction_specific_types
    type t_particle_results
       real(kind_physics), dimension(3) :: u   ! velocities
       real(kind_physics), dimension(3) :: af  ! RHS for vorticity/alpha ODE
+      real(kind_physics), dimension(3) :: psi ! stream function
       real(kind_physics) :: div               ! divergence
    end type t_particle_results
-   integer, private, parameter :: nprops_particle_results = 3
+   integer, private, parameter :: nprops_particle_results = 4
 
-   type(t_particle_results), parameter :: EMPTY_PARTICLE_RESULTS = t_particle_results([0., 0., 0.], [0., 0., 0.], 0.)
+   type(t_particle_results), parameter :: EMPTY_PARTICLE_RESULTS = t_particle_results([0., 0., 0.], [0., 0., 0.], [0., 0., 0.], 0.)
 
    !> Data structure for storing multiple moments of tree nodes
    type t_tree_node_interaction_data
@@ -121,12 +122,13 @@ contains
       call MPI_TYPE_COMMIT(mpi_type_particle_data, ierr)
 
       ! register results data type
-      blocklengths(1:nprops_particle_results) = [3, 3, 1]  !&
-      types(1:nprops_particle_results)        = [MPI_REAL8, MPI_REAL8, MPI_REAL8]  !&
+      blocklengths(1:nprops_particle_results) = [3, 3, 3, 1]  !&
+      types(1:nprops_particle_results)        = [MPI_REAL8, MPI_REAL8, MPI_REAL8, MPI_REAL8]  !&
       call MPI_GET_ADDRESS(dummy_particle_results,      address(0), ierr)  !&
       call MPI_GET_ADDRESS(dummy_particle_results%u,    address(1), ierr)  !&
       call MPI_GET_ADDRESS(dummy_particle_results%af,   address(2), ierr)  !&
-      call MPI_GET_ADDRESS(dummy_particle_results%div,  address(3), ierr)  !&
+      call MPI_GET_ADDRESS(dummy_particle_results%psi,  address(3), ierr)  !&
+      call MPI_GET_ADDRESS(dummy_particle_results%div,  address(4), ierr)  !&
       displacements(1:nprops_particle_results) = int(address(1:nprops_particle_results) - address(0))
       call MPI_TYPE_CREATE_STRUCT(nprops_particle_results, blocklengths, displacements, types, mpi_type_particle_results, ierr)
       call MPI_TYPE_COMMIT(mpi_type_particle_results, ierr)
