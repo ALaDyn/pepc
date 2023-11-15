ALaDyn/pepc

commit e2e3fc542c3e171314c42007d06840a83b6d9670

Some more modern Fortran for [pepc-dvh]
Remove calls to `system` which may be problematic at some point, more
importantly remove `GOTO` statements.

src/frontends/pepc-dvh/physvars.f90
@@ -213,13 +213,12 @@ contains
       ! 3D correction
       alpha = 1.d0
 
-100   continue
       diff_error = erfc(1.d0 / sqrt(alpha)) + 2.d0 * exp(-1.d0 / alpha) / sqrt(alpha * pi)
-
-      if (diff_error .gt. eps_diff) then
+      do while (diff_error .gt. eps_diff) then
          alpha = 0.95d0 * alpha
-         goto 100
-      end if
+         diff_error = erfc(1.d0 / sqrt(alpha)) + 2.d0 * exp(-1.d0 / alpha) / sqrt(alpha * pi)
+      end do
+
       if (my_rank .eq. 0) write (*, *) 'Diffusion error (csi) ', diff_error, alpha, alpha * Rd * Rd / 4.d0 / nu
 
       Delta_tdiff = alpha * Rd * Rd / 4.d0 / nu

src/frontends/pepc-dvh/files.f90
@@ -25,21 +25,17 @@ module files
 
 contains
 
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !>
    !>   Open plain text output files
    !>
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    subroutine openfiles
       use physvars
       use module_utils
       use mpi
-      integer :: linenum_e, linenum_d, ierr, ierr1, ierr2
-      Character*2 :: restart_num
-      Character*99 :: dum_en, dum_di
-
-      ierr1 = 0
-      ierr2 = 0
+      integer :: num_enstro_files, num_diag_files, ierr
+      character(len=99) :: enstro_filename, diag_filename
 
       if (my_rank .eq. 0) then
 
@@ -48,42 +44,40 @@ contains
          open (newunit=dom_unit, file='domains.dat')
 
          if (file_exists('ener_enstro.dat')) then
+            ! Create restart output files that include a number and start with 'R'
 
-            call system("ls *enstro.dat > TMP_list")
-            call read_line_num('TMP_list', dum_en, linenum_e)
+            do num_enstro_files = 1, 99 ! the initial choice is to have a two-digit option here
+               write (enstro_filename, '(a,i2.2,a)') 'R', num_enstro_files, '_ener_enstro.dat'
+               if (.not. file_exists(trim(enstro_filename))) exit
+            end do
 
-            call system("ls *linear_diag.dat > TMP_list")
-            call read_line_num('TMP_list', dum_di, linenum_d)
+            do num_diag_files = 1, 99 ! the initial choice is to have a two-digit option here
+               write (diag_filename, '(a,i2.2,a)') 'R', num_diag_files, '_linear_diag.dat'
+               if (.not. file_exists(trim(diag_filename))) exit
+            end do
 
-            call system("rm -fr TMP_list")
-
-            if (linenum_e .eq. linenum_d) then
-               write (restart_num, '(I2.2)') linenum_e
-            else
+            if (num_enstro_files .ne. num_diag_files) then
                call graceful_abort('Number of energy files differs from number of diagnostics files'//achar(13)//achar(10)// &
                                    'Check files ener_enstro.dat and linear_diag.dat in the working directory and restart')
             end if
 
-            write (*, '(A)') 'Last energy diagnostic file found: ', dum_en
-            write (*, '(A)') 'Attempting to create new file: R'//trim(restart_num)//'_ener_enstro.dat'
-            open (newunit=ener_unit, file='R'//trim(restart_num)//'_ener_enstro.dat', STATUS='NEW', IOSTAT=ierr1)
-            write (*, '(A)')
-            write (*, '(A)') 'Last linear diagnostic file found: ', dum_di
-            write (*, '(A)') 'Attempting to create new file: R'//trim(restart_num)//'_linear_diag.dat'
-            open (newunit=diag_unit, file='R'//trim(restart_num)//'_linear_diag.dat', STATUS='NEW', IOSTAT=ierr2)
+            write (*, '(a,1x,i2,1x,a)') 'Found', num_enstro_files, 'energy diagnostic file(s)' ! this includes the non-restart file
+            write (*, '(a)') 'Attempting to create new file: '//trim(enstro_filename)
+            open (newunit=ener_unit, file=trim(enstro_filename), status='new', iostat=ierr)
+            if (ierr .ne. 0) call graceful_abort('Error creating file '//trim(diag_filename)//achar(13)//achar(10)// &
+                                                 'File already exists. Change name or delete it.')
+            write (*, '(a)')
+            write (*, '(a,1x,i2,1x,a)') 'Found', num_diag_files, 'linear diagnostic file(s)' ! this includes the non-restart file
+            write (*, '(a)') 'Attempting to create new file: '//trim(diag_filename)
+            open (newunit=diag_unit, file=trim(diag_filename), status='new', iostat=ierr)
+            if (ierr .ne. 0) call graceful_abort('Error creating file '//trim(diag_filename)//achar(13)//achar(10)// &
+                                                 'File already exists. Change name or delete it.')
          else
-            open (newunit=ener_unit, file='ener_enstro.dat', STATUS='NEW')
-            open (newunit=diag_unit, file='linear_diag.dat', STATUS='NEW')
+            ! create first output file
+            open (newunit=ener_unit, file='ener_enstro.dat', status='new')
+            open (newunit=diag_unit, file='linear_diag.dat', status='new')
          end if
 
-      end if
-
-      if (ierr1 .ne. 0) then
-         call graceful_abort('Error creating file R'//trim(restart_num)//'_ener_enstro.dat'//achar(13)//achar(10)// &
-                             'File already exists. Change name or delete it.')
-      else if (ierr2 .ne. 0) then
-         call graceful_abort('Error creating file R'//trim(restart_num)//'_linear_diag.dat'//achar(13)//achar(10)// &
-                             'File already exists. Change name or delete it.')
       end if
 
       ! for MPI I/O
@@ -94,7 +88,7 @@ contains
    subroutine graceful_abort(message)
       use mpi
       character(*), intent(in)  :: message
-      integer                     :: ierr
+      integer                   :: ierr
 
       write (*, '(A)') '#############################################################'
       write (*, '(A)')
@@ -105,28 +99,11 @@ contains
       call MPI_ABORT(MPI_COMM_WORLD, 1, ierr)
    end subroutine graceful_abort
 
-   subroutine read_line_num(file_name, dum, linenum)
-      integer, intent(out) :: linenum
-      character(*), intent(in)  :: file_name
-      character*99, intent(out) :: dum
-
-      linenum = 0
-
-      Open (1, file=file_name)
-10    continue
-      read (1, *, err=20, end=20) dum
-      linenum = linenum + 1
-      goto 10
-20    continue
-      close (1)
-
-   end subroutine read_line_num
-
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !>
    !>   Close plain text output files
    !>
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    subroutine closefiles
       use physvars
 
@@ -142,11 +119,11 @@ contains
 
    end subroutine closefiles
 
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !>
    !>   Dump VTK
    !>
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    subroutine dump(i, simtime)
 
       use physvars
@@ -179,11 +156,11 @@ contains
 
    end subroutine dump
 
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !>
    !>   Dump checkpoint
    !>
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    subroutine write_checkpoint(i, simtime)
 
       use physvars
@@ -273,11 +250,11 @@ contains
 
    end subroutine dump_results
 
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !>
    !>   Read in data from MPI checkpoint file
    !>
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    subroutine read_in_checkpoint
 
       use physvars
@@ -298,11 +275,11 @@ contains
 
    end subroutine read_in_checkpoint
 
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !>
    !>   Dump particles to binary parallel VTK
    !>
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    subroutine write_particles_to_vtk(step, time)
 
       use physvars

commit 57de71a512a271dd1f35526f75df3f701cee71d3

Replace signal handler w/ wallclock [pepc-dvh]
The signal handler appears to be an extension to the standard and used
differently in GCC and Intel compilers, so removing it.
Adding another input parameter to provide the wallclock limit (in
minutes!) to `pepc-dvh`, checking the actual runtime against this limit.

This fixes #10 and in part reverts f9b30420 by implementing an
alternative already mentioned in #9.

Also fixing a compilation error introduced with e2e3fc542.

src/frontends/pepc-dvh/pepc.f90
@@ -46,13 +46,13 @@ program pepcdvh
    implicit none
 
    integer(kind_particle) :: i
-   real :: trun                     ! total run time including restarts and offset
+   real :: trun ! total run time including restarts and offset
    integer :: itime, stage, t_flag
-   logical :: signal_caught
+   integer(int64) :: start_ticks, ticks_per_minute ! ticks when starting and per minute
 
-   ! Register signal handler for signal SIGUSR1 (10)
-   call signal(10, handle_wallclock)
-   signal_caught = .false.
+   ! Register starting time (effectively in minutes)
+   call system_clock(start_ticks, ticks_per_minute)
+   ticks_per_minute = ticks_per_minute * 60 ! The call returns ticks per second, so convert to minutes
 
    ! Allocate array space for tree
    call pepc_initialize("pepc-dvh", my_rank, n_cpu, .true.)
@@ -78,7 +78,7 @@ program pepcdvh
    t_out = ts + n_out * dt_out
 
    ! Loop over all timesteps unless we caught a signal
-   do while (itime .lt. nt .and. .not. signal_caught)
+   do while (itime .lt. nt .and. .not. wallclock_limit_near(10))
 
       call timer_reset(t_io)
 
@@ -180,20 +180,35 @@ program pepcdvh
    call pepc_finalize()
 
 contains
-   subroutine handle_wallclock()
-      ! Exit gracefully in case SLURM signals the wallclock limit is close.
-      ! To achieve this, we toggle a flag when we see signal 10 (SIGUSR1).
+   logical function wallclock_limit_near(margin_in)
+      ! Compare runtime with the provided wallclock limit (input parameter) and
+      ! flag if we are within a provided margin (in minutes)
+      ! TODO
+      ! This function does not handle COUNT_MAX that may reset counters...
+      integer, intent(in), optional :: margin_in ! margin between current time and wallclock limit
+      integer :: margin
+      integer(int64) :: current_ticks
 
-      if (my_rank .eq. 0) then
-         write (*, *) '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-         write (*, *) '!!! CAUGHT SIGNAL FROM SLURM, WILL BE STOPPING... !!!'
-         write (*, *) '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-
+      if (present(margin_in)) then
+         margin = margin_in
+      else
+         margin = 10 ! default to 10 minutes
       end if
 
-      signal_caught = .true.
+      wallclock_limit_near = .false.
+      call system_clock(current_ticks)
+
+      if (((current_ticks - start_ticks) / ticks_per_minute + margin) .gt. wall_mins) then
+         wallclock_limit_near = .true.
+         if (my_rank .eq. 0) then
+            write (*, *) '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+            write (*, *) '!!! CLOSE TO WALLCLOCK LIMIT, WILL BE STOPPING... !!!'
+            write (*, *) '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+         end if
+      end if
+
       return
 
-   end subroutine handle_wallclock
+   end function wallclock_limit_near
 end program pepcdvh
 #endif

src/frontends/pepc-dvh/physvars.f90
@@ -67,6 +67,7 @@ module physvars
    integer :: nt                   ! # timesteps and current timestep
    integer :: rk_stages            ! order of the Runge-Kutta time integration scheme
    logical :: vort_check           ! Control of total vorticity during remeshing
+   integer :: wall_mins            ! Wallclock limit from batch system IN MINUTES
 
    ! I/O stuff
    integer :: ifile_cpu            ! O/P stream
@@ -145,7 +146,8 @@ contains
       namelist /pepcv/ n, ispecial, ts, te, nu, Co, nv_on_Lref, rk_stages, &    !&
                        h, Delta_r, thresh, Uref, Lref, &                        !&
                        rmax, r_torus, nc, nphi, g, torus_offset, n_in, &        !&
-                       dump_time, cp_time, input_itime, nDeltar, vort_check     !&
+                       wall_mins, dump_time, cp_time, input_itime, nDeltar, &   !&
+                       vort_check                                               !&
 
       !  Default input set
       ispecial     = 1                                                          !&
@@ -176,8 +178,9 @@ contains
 
       dump_time    = 1                                                          !&
       cp_time      = 0                                                          !&
-      t_out       = 0.                                                          !&
+      t_out        = 0.                                                         !&
       dt_out       = 0.                                                         !&
+      wall_mins    = 24 * 60 !& default to 24 hrs                               !&
 
       ! Check total vorticity
       vort_check   = .true.                                                     !&
@@ -214,7 +217,7 @@ contains
       alpha = 1.d0
 
       diff_error = erfc(1.d0 / sqrt(alpha)) + 2.d0 * exp(-1.d0 / alpha) / sqrt(alpha * pi)
-      do while (diff_error .gt. eps_diff) then
+      do while (diff_error .gt. eps_diff)
          alpha = 0.95d0 * alpha
          diff_error = erfc(1.d0 / sqrt(alpha)) + 2.d0 * exp(-1.d0 / alpha) / sqrt(alpha * pi)
       end do